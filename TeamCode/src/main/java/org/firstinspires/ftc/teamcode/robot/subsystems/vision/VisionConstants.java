42a9e5c5613e25805cfe27be80d5ef9ca65ef76b
Jonathan Liu <jonliu168@gmail.com>
2025-09-13 20:37:18 -0700

    structure for vision pipeline based on the openftc example for easyopencv

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/VisionConstants.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems.vision;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VisionConstants {
+    public static class APRILTAG {
+        Map<Integer, String> tagMap = new HashMap<Integer, String>() {{
+            put(20, "BLUESCORE");
+            put(21, "GPP");
+            put(22, "PGP");
+            put(23,"PPG");
+            put(24, "REDSCORE");
+        }};
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/VisionPipeline.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems.vision;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class VisionPipeline extends OpenCvPipeline
+{
+
+    private AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+            .setDrawAxes(true)
+            .setDrawCubeProjection(true)
+            .setDrawTagID(true)
+            .setDrawTagOutline(true)
+            .setSuppressCalibrationWarnings(false)
+            .setOutputUnits(DistanceUnit.INCH, AngleUnit.RADIANS)
+            .setNumThreads(3)
+            .build();
+
+
+    public VisionPipeline()
+    {
+        this.vPortalBuilder.addProcessor(this.tagProcessor);
+    }
+    boolean viewportPaused;
+    private final VisionPortal.Builder vPortalBuilder = new VisionPortal.Builder();
+
+        return this;
+
+
+    @Override
+    public Mat processFrame(Mat input)
+    {
+        Imgproc.rectangle(
+                input,
+                new Point(
+                        input.cols()/4,
+                        input.rows()/4),
+                new Point(
+                        input.cols()*(3f/4f),
+                        input.rows()*(3f/4f)),
+                new Scalar(0, 255, 0), 4);
+
+
+        return input;
+    }
+
+    @Override
+    public void onViewportTapped()
+    {
+
+        viewportPaused = !viewportPaused;
+
+        if(viewportPaused)
+        {
+            webcam.pauseViewport();
+        }
+        else
+        {
+            webcam.resumeViewport();
+        }
+    }
+}
