1cb743a4da4a1d66e245ddcc14b523fc4a4758c1
tarunja1ks <tarunjaiks@gmail.com>
2025-09-28 09:43:09 -0700

    tuner files i know so far

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/shooterTuner.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.opmodes.tuning;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robot.init.Robot;
+import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
+
+@Config
+@TeleOp(group="Shooter Tuner")
+public class shooterTuner extends LinearOpMode {
+
+    private final RobotHardware hardware = new RobotHardware();
+    private final Robot robot = new Robot();
+
+    public static double goalSpeed = 0.0;
+
+    @Override
+    public void runOpMode() {
+        hardware.init(hardwareMap, LynxModule.BulkCachingMode.MANUAL);
+        robot.init(hardware, telemetry);
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Manually clear the bulk read cache. Deleting this would be catastrophic b/c stale
+            // vals would be used.
+            for (LynxModule hub : hardware.allHubs) {
+                hub.clearBulkCache();
+            }
+
+            robot.shooter.setSpeed(goalSpeed);
+
+            robot.spindexer.periodic();
+
+
+            hardware.write();
+
+
+            robot.telemetry.addData("Yaw of Spindexer",goalSpeed);
+            robot.telemetry.addData("Current angle",robot.spindexer.getPosition());
+            robot.telemetry.update();
+
+        }
+
+    }
+
+}
