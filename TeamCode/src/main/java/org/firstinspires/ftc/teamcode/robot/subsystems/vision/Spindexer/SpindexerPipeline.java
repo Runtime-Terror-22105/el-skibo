46792a1f8a6ee2b6a5b126d85c3eab15eb15a437
Jonathan Liu <jonliu168@gmail.com>
2025-09-28 15:11:01 -0700

    spindexer pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/Spindexer/SpindexerPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/Spindexer/SpindexerPipeline.java
@@ -7,10 +7,12 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.CvType;
 import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.MatOfPoint3f;
 import org.opencv.core.Point;
 import org.opencv.core.Point3;
+import org.opencv.imgproc.Moments;
 import org.openftc.apriltag.AprilTagDetection;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -22,6 +24,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import org.firstinspires.ftc.robotcore.external.navigation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SpindexerPipeline extends OpenCvPipeline
 {
@@ -31,13 +34,18 @@ public class SpindexerPipeline extends OpenCvPipeline
     private final Mat hsv = new Mat();
     private Mat output = new Mat();
 
+
+
     public static Scalar purpleLow1  = new Scalar(45.3, 77.9, 155.8);
     public static Scalar purpleHigh1 = new Scalar(121.8, 68, 255);
     public static Scalar purpleLow2  = new Scalar(117.6, 59.5, 97.8);
     public static Scalar purpleHigh2 = new Scalar(168.6, 255, 255);
 
-    public static Scalar greenLow  = new Scalar(29.8, 89.3, 19.8);
-    public static Scalar greenHigh = new Scalar(59.5, 144.5, 158.7);
+//    public static Scalar greenLow  = new Scalar(29.8, 89.3, 19.8);
+//    public static Scalar greenHigh = new Scalar(59.5, 144.5, 158.7);
+public static Scalar greenLow  = new Scalar(35, 40, 40);
+    // Upper bound for green
+    public static Scalar greenHigh = new Scalar(85, 255, 255);
     Mat purpleMask1 = new Mat();
     Mat purpleMask2 = new Mat();
     Mat purpleMask = new Mat();
@@ -53,51 +61,23 @@ public class SpindexerPipeline extends OpenCvPipeline
     public static double cx = 402.145;
     public static double cy = 221.506;
 
-    static final double FEET_PER_METER = 3.28084;
-
-    public static double TAG_SIZE = 0.166;
-
-    private long aprilTagDetector;
-
-    private ArrayList<org.openftc.apriltag.AprilTagDetection> detections = new ArrayList<>();
 
     private OpenCvCamera camera;
 
     private float decimation;
     private boolean needToSetDecimation;
     private final Object decimationSync = new Object();
+
+    private final List<Point> detectedCenters = new ArrayList<>();
     Telemetry telemetry;
 
     public SpindexerPipeline(Telemetry telemetry) {
         this.telemetry = telemetry;
-        constructMatrix();
     }
-    boolean viewportPaused;
     private final VisionPortal.Builder vPortalBuilder = new VisionPortal.Builder();
 
     @Override
-    public void init(Mat frame)
-    {
-        aprilTagDetector = AprilTagDetectorJNI.createApriltagDetector(AprilTagDetectorJNI.TagFamily.TAG_36h11.string, 3, 3);
-    }
-
-    @Override
     public Mat processFrame(Mat input) {
-        Imgproc.cvtColor(input, grey, Imgproc.COLOR_RGBA2GRAY);
-
-        synchronized (decimationSync)
-        {
-            if(needToSetDecimation)
-            {
-                AprilTagDetectorJNI.setApriltagDetectorDecimation(aprilTagDetector, decimation);
-                needToSetDecimation = false;
-            }
-        }
-        aprilTagDetector = AprilTagDetectorJNI.createApriltagDetector(AprilTagDetectorJNI.TagFamily.TAG_36h11.string, 3, 3);
-        synchronized (detectionsUpdateSync)
-        {
-            detectionsUpdate = detections;
-        }
         Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
         Core.inRange(hsv, purpleLow1, purpleHigh1, purpleMask1);
         Core.inRange(hsv, purpleLow2, purpleHigh2, purpleMask2);
@@ -107,182 +87,62 @@ public class SpindexerPipeline extends OpenCvPipeline
         Mat combinedMask = new Mat();
         Core.bitwise_or(purpleMask, greenMask, combinedMask);
 
+        // Find contours (blobs)
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(combinedMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // Clear old centers
+        detectedCenters.clear();
+
+        double minArea = 500.0; // tune this for noise filtering
+
+        for (MatOfPoint contour : contours) {
+            double area = Imgproc.contourArea(contour);
+            if (area > minArea) {
+                Moments m = Imgproc.moments(contour);
+                if (m.m00 != 0) {
+                    int cx = (int)(m.m10 / m.m00);
+                    int cy = (int)(m.m01 / m.m00);
+                    Point center = new Point(cx, cy);
+                    String point = "";
+
+                    if(center.y < (double)input.height()/2)
+                    {
+                        point = "up";
+                    }
+                    else if(center.x < (double)input.width()/2)
+                    {
+                        point = "left";
+                    }
+                    else
+                    {
+                        point = "right";
+                    }
+
+                    // Determine color
+                    String colorLabel = "Unknown";
+                    if (purpleMask.get(cy, cx)[0] > 0) {
+                        colorLabel = "Purple" + point;
+                    } else if (greenMask.get(cy, cx)[0] > 0) {
+                        colorLabel = "Green" + point;
+                    }
+
+                    // Save center
+                    detectedCenters.add(center);
+                    Imgproc.circle(input, center, 5, new Scalar(255, 0, 0), -1);
+                    Imgproc.putText(input, colorLabel, center, Imgproc.FONT_HERSHEY_SIMPLEX, 0.7, new Scalar(255, 255, 255), 2);
+
+
+                    if (telemetry != null) {
+                        telemetry.addData("Blob", "%s at (%d, %d)", colorLabel, cx, cy);
+                    }
+                }
+            }
+        }
+
         Mat masked = new Mat();
         Core.bitwise_and(input, input, masked, combinedMask);
-        detections = AprilTagDetectorJNI.runAprilTagDetectorSimple(aprilTagDetector, grey, TAG_SIZE, fx, fy, cx, cy);
-        for(AprilTagDetection detection : detections)
-        {
-            Pose pose = poseFromTrapezoid(detection.corners, cameraMatrix, TAG_SIZE, TAG_SIZE);
-            drawAxisMarker(input, TAG_SIZE/2.0, 6, pose.rvec, pose.tvec, cameraMatrix);
-            draw3dCubeMarker(input, TAG_SIZE, TAG_SIZE, TAG_SIZE, 5, pose.rvec, pose.tvec, cameraMatrix);
-
-            Orientation rot = Orientation.getOrientation(detection.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
-
-            telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
-            telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
-            telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
-            telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
-
-            telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", rot.firstAngle));
-            telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", rot.secondAngle));
-            telemetry.addLine(String.format("Rotation Roll: %.2f degrees", rot.thirdAngle));
-        }
         return masked;
-//        ArrayList<AprilTagDetection> detections = tagProcessor.getDetections();
-//
-//        for (AprilTagDetection tag : detections) {
-//            Point[] corners = tag.corners;
-//            for (int i = 0; i < 4; i++) {
-//                Imgproc.line(input, corners[i], corners[(i + 1) % 4], new Scalar(0, 255, 0), 2);
-//            }
-//            Imgproc.putText(input, "ID:" + tag.id, corners[0], Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0,0,255), 2);
-//        }
-//
-//        return input;
-    }
-
-    void constructMatrix()
-    {
-        //     Construct the camera matrix.
-        //
-        //      --         --
-        //     | fx   0   cx |
-        //     | 0    fy  cy |
-        //     | 0    0   1  |
-        //      --         --
-        //
-
-        cameraMatrix = new Mat(3,3, CvType.CV_32FC1);
-
-        cameraMatrix.put(0,0, fx);
-        cameraMatrix.put(0,1,0);
-        cameraMatrix.put(0,2, cx);
-
-        cameraMatrix.put(1,0,0);
-        cameraMatrix.put(1,1,fy);
-        cameraMatrix.put(1,2,cy);
-
-        cameraMatrix.put(2, 0, 0);
-        cameraMatrix.put(2,1,0);
-        cameraMatrix.put(2,2,1);
-    }
-
-    class Pose
-    {
-        Mat rvec;
-        Mat tvec;
-
-        public Pose()
-        {
-            rvec = new Mat();
-            tvec = new Mat();
-        }
-
-        public Pose(Mat rvec, Mat tvec)
-        {
-            this.rvec = rvec;
-            this.tvec = tvec;
-        }
-    }
-
-    void drawAxisMarker(Mat buf, double length, int thickness, Mat rvec, Mat tvec, Mat cameraMatrix)
-    {
-        // The points in 3D space we wish to project onto the 2D image plane.
-        // The origin of the coordinate space is assumed to be in the center of the detection.
-        MatOfPoint3f axis = new MatOfPoint3f(
-                new Point3(0,0,0),
-                new Point3(length,0,0),
-                new Point3(0,length,0),
-                new Point3(0,0,-length)
-        );
-
-        // Project those points
-        MatOfPoint2f matProjectedPoints = new MatOfPoint2f();
-        Calib3d.projectPoints(axis, rvec, tvec, cameraMatrix, new MatOfDouble(), matProjectedPoints);
-        Point[] projectedPoints = matProjectedPoints.toArray();
-
-        // Draw the marker!
-        Imgproc.line(buf, projectedPoints[0], projectedPoints[1], red, thickness);
-        Imgproc.line(buf, projectedPoints[0], projectedPoints[2], green, thickness);
-        Imgproc.line(buf, projectedPoints[0], projectedPoints[3], blue, thickness);
-
-        Imgproc.circle(buf, projectedPoints[0], thickness, white, -1);
-    }
-
-    void draw3dCubeMarker(Mat buf, double length, double tagWidth, double tagHeight, int thickness, Mat rvec, Mat tvec, Mat cameraMatrix)
-    {
-        //axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
-        //       [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
-
-        // The points in 3D space we wish to project onto the 2D image plane.
-        // The origin of the coordinate space is assumed to be in the center of the detection.
-        MatOfPoint3f axis = new MatOfPoint3f(
-                new Point3(-tagWidth/2, tagHeight/2,0),
-                new Point3( tagWidth/2, tagHeight/2,0),
-                new Point3( tagWidth/2,-tagHeight/2,0),
-                new Point3(-tagWidth/2,-tagHeight/2,0),
-                new Point3(-tagWidth/2, tagHeight/2,-length),
-                new Point3( tagWidth/2, tagHeight/2,-length),
-                new Point3( tagWidth/2,-tagHeight/2,-length),
-                new Point3(-tagWidth/2,-tagHeight/2,-length));
-
-        // Project those points
-        MatOfPoint2f matProjectedPoints = new MatOfPoint2f();
-        Calib3d.projectPoints(axis, rvec, tvec, cameraMatrix, new MatOfDouble(), matProjectedPoints);
-        Point[] projectedPoints = matProjectedPoints.toArray();
-
-        // Pillars
-        for(int i = 0; i < 4; i++)
-        {
-            Imgproc.line(buf, projectedPoints[i], projectedPoints[i+4], blue, thickness);
-        }
-
-        // Base lines
-        //Imgproc.line(buf, projectedPoints[0], projectedPoints[1], blue, thickness);
-        //Imgproc.line(buf, projectedPoints[1], projectedPoints[2], blue, thickness);
-        //Imgproc.line(buf, projectedPoints[2], projectedPoints[3], blue, thickness);
-        //Imgproc.line(buf, projectedPoints[3], projectedPoints[0], blue, thickness);
-
-        // Top lines
-        Imgproc.line(buf, projectedPoints[4], projectedPoints[5], green, thickness);
-        Imgproc.line(buf, projectedPoints[5], projectedPoints[6], green, thickness);
-        Imgproc.line(buf, projectedPoints[6], projectedPoints[7], green, thickness);
-        Imgproc.line(buf, projectedPoints[4], projectedPoints[7], green, thickness);
-    }
-
-    Pose poseFromTrapezoid(Point[] points, Mat cameraMatrix, double tagsizeX , double tagsizeY)
-    {
-        // The actual 2d points of the tag detected in the image
-        MatOfPoint2f points2d = new MatOfPoint2f(points);
-
-        // The 3d points of the tag in an 'ideal projection'
-        Point3[] arrayPoints3d = new Point3[4];
-        arrayPoints3d[0] = new Point3(-tagsizeX/2, tagsizeY/2, 0);
-        arrayPoints3d[1] = new Point3(tagsizeX/2, tagsizeY/2, 0);
-        arrayPoints3d[2] = new Point3(tagsizeX/2, -tagsizeY/2, 0);
-        arrayPoints3d[3] = new Point3(-tagsizeX/2, -tagsizeY/2, 0);
-        MatOfPoint3f points3d = new MatOfPoint3f(arrayPoints3d);
-
-        // Using this information, actually solve for pose
-        Pose pose = new Pose();
-        Calib3d.solvePnP(points3d, points2d, cameraMatrix, new MatOfDouble(), pose.rvec, pose.tvec, false);
-
-        return pose;
-    }
-
-    @Override
-    public void onViewportTapped()
-    {
-//
-//        viewportPaused = !viewportPaused;
-//
-//        if(viewportPaused)
-//        {
-//            camera.pauseViewport();
-//        }
-//        else
-//        {
-//            camera.resumeViewport();
-//        }
     }
 }
