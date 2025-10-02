888fe12138797bcfbe5d37e18ee21fe6822cde41
Jonathan Liu <jonliu168@gmail.com>
2025-10-01 21:40:19 -0700

    move vportal stuff out but its a basis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/CameraSubsystem.java
@@ -1,32 +1,24 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems.vision;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.robot.subsystems.vision.AprilTag.AprilTagPipeline;
 import org.firstinspires.ftc.teamcode.robot.subsystems.vision.Spindexer.SpindexerPipeline;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.VisionPortal;
 import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvWebcam;
-
-import java.util.ArrayList;
-
+import org.openftc.easyopencv.OpenCvPipeline;
 @TeleOp
 public class CameraSubsystem extends SubsystemBase
 {
-    private OpenCvWebcam webcam;
-
-    private SpindexerPipeline pipeline = new SpindexerPipeline(webcam);
-
-    private ArrayList<AprilTagDetection> detections;
+    private OpenCvCamera aprilTagCamera;
+    private OpenCvCamera spindexerCamera;
     public enum GLYPH {
         GPP,PGP,PPG
     }
+
+    Telemetry telemetry;
 
     private GLYPH gameGlyph;
     private boolean decodedGlyph = false; //when the movie uses the title of the movie
@@ -36,17 +28,21 @@ public class CameraSubsystem extends SubsystemBase
         return gameGlyph;
     }
 
-    @Override
-    public void periodic() {
+    private final VisionPortal.Builder vPortalBuilder = new VisionPortal.Builder();
 
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        webcam.setPipeline(pipeline);
-        webcam.setMillisecondsPermissionTimeout(5000);
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+    public CameraSubsystem() {
+        initCamera(aprilTagCamera, new AprilTagPipeline(telemetry));
+        initCamera(spindexerCamera, new SpindexerPipeline(telemetry));
+        //vPortalBuilder.setCamera( hardware map camera names whenever they are determined
+        vPortalBuilder.build();
+    }
+
+    private void initCamera(OpenCvCamera camera,OpenCvPipeline pipeline) {
+        aprilTagCamera.setPipeline(pipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
-                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
@@ -54,36 +50,11 @@ public class CameraSubsystem extends SubsystemBase
 
             }
         });
-            detections = pipeline.getSeenTags();
-            if(detections.size() != 0)
-            {
-                for(AprilTagDetection tag : detections)
-                {
-                    if(tag.id >= 21 && tag.id <= 23 && !decodedGlyph)
-                    {
-                        gameGlyph = GLYPH.valueOf(VisionConstants.APRILTAG.tagMap.get(tag.id));
-                        //decodedGlyph = true;
-                    }
-                    telemetry.addData("Tag ID",tag.id);
-                    telemetry.addData("Nickname Tag", VisionConstants.APRILTAG.tagMap.get(tag.id));
-                    //technically unsafe if the tag is somehow falsely read or someone in the audience wears an apriltag shirt
-                    telemetry.addData("Pose X", tag.robotPose.getPosition().x); //i have NO idea what this returns ehl em ay oh
-                    telemetry.addData("Pose Y", tag.robotPose.getPosition().y);
-                    telemetry.addData("Pose Z", tag.robotPose.getPosition().z);
-                }
-            }
-            telemetry.addData("Frame Count", webcam.getFrameCount());
-            telemetry.addData("FPS", webcam.getFps());
-            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
-            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
-            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
-            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+    }
 
+    @Override
+    public void periodic() {
 
             telemetry.update();
-
-            if (gamepad1.a) {
-                webcam.stopStreaming();
-            }
         }
     }
