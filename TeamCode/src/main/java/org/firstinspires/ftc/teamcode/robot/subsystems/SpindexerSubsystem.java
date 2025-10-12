dd58a1f6b3639d0712b2e6540132d43db21aaebf
Landon <53448214+lunbun@users.noreply.github.com>
2025-10-12 10:59:20 -0700

    make command to enable/disable spindexer transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
@@ -1,24 +1,21 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 import org.firstinspires.ftc.teamcode.math.controllers.PidfController;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
 
+@Config
 public class SpindexerSubsystem extends SubsystemBase {
+    public static double RAMP_DOWN_POSITION = 1.0; // transfer ramp down
+    public static double RAMP_UP_POSITION = 0.0; // transfer ramp up
+
     private final RobotHardware hardware;
 
-    public static double activatePosition=1.0; // cams up
+    private double rampPosition = RAMP_UP_POSITION;
 
-    public static double deactivatePosition=0.0; // cams down
-
-    public double SHOOTER_INTAKE_SPEED=0.0; // this is the speed where the shooter melonbotic servo intakes the balls
-
-    public static double SHOOTER_INTAKING_SPEED=1.0;
-
-    public double PopperPosition=deactivatePosition;
-
-    public double spindexerPower=0.0;
+    public double spindexerPower = 0.0;
     public static PidfController.PidfCoefficients turningPidCoefficients =
             new PidfController.PidfCoefficients(0.014, 0, 0, 1, 0);
     public static double yawPidTolerance = 0.1;
@@ -31,13 +28,13 @@ public class SpindexerSubsystem extends SubsystemBase {
         this.yawPid.setTargetPosition(0.0);
     }
 
-    public void setYaw(double angle){ //angle is in radians cuz i said so oh yeah and also have todo: optimization like the swerve pod thingy where u do the shortest distance
+    public void setYaw(double angle) { //angle is in radians cuz i said so oh yeah and also have todo: optimization like the swerve pod thingy where u do the shortest distance
         this.yawPid.setTargetPosition(angle);
     }
 
     public void updateSpindexer(){
 //        if(hardware.spindexerEncoder.getCurrentPosition())
-        this.spindexerPower= yawPid.calculatePower(hardware.spindexerEncoder.getCurrentPosition(),0);
+        this.spindexerPower = yawPid.calculatePower(hardware.spindexerEncoder.getCurrentPosition(),0);
         // setting pid power into the spindexer
     }
 
@@ -45,24 +42,17 @@ public class SpindexerSubsystem extends SubsystemBase {
         return hardware.spindexerEncoder.getCurrentPosition();
     }
 
-    public void activateTransfer(){
-        this.PopperPosition=this.activatePosition;
-        this.SHOOTER_INTAKE_SPEED=this.SHOOTER_INTAKING_SPEED;
-
+    public void activateTransfer() {
+        this.rampPosition = SpindexerSubsystem.RAMP_DOWN_POSITION;
     }
 
-    public void deactivateTransfer(){
-        this.PopperPosition=this.deactivatePosition;
-        this.SHOOTER_INTAKE_SPEED=0.0;
+    public void deactivateTransfer() {
+        this.rampPosition = SpindexerSubsystem.RAMP_UP_POSITION;
     }
-
-
-
-
 
     @Override
     public void periodic() {
-        this.hardware.spindexerTransferRamp.setPosition(this.PopperPosition);
+        this.hardware.spindexerTransferRamp.setPosition(this.rampPosition);
         this.updateSpindexer();
         this.hardware.spindexerRotate.setPower(this.spindexerPower);
     }
