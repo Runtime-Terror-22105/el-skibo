a13bd4d46fd7e74c3b2824655b36d152749cf7e5
tarunja1ks <tarunjaiks@gmail.com>
2025-11-01 18:32:58 -0700

    tiny issue fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/CameraSubsystem.java
@@ -19,9 +19,6 @@ import java.util.ArrayList;
 @TeleOp
 public class CameraSubsystem extends SubsystemBase
 {
-    private OpenCvCamera aprilTagCamera;
-    private OpenCvCamera spindexerCamera;
-
     public enum GLYPH {
         GPP,PGP,PPG
     }
