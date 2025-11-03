de4dbb19cc82f4562c3f8db252dcf7570c011490
aaguy-hue <aaditmathur6@gmail.com>
2025-11-03 09:15:59 -0800

    refactor spindexer homing command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/shooter/SpindexerHoming.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/shooter/SpindexerHoming.java
@@ -1,17 +1,38 @@
 package org.firstinspires.ftc.teamcode.robot.command.shooter;
 
+import com.seattlesolvers.solverslib.command.CommandBase;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 
 import org.firstinspires.ftc.teamcode.robot.subsystems.SpindexerSubsystem;
 
-public class SpindexerHoming extends InstantCommand {
+public class SpindexerHoming extends CommandBase {
 
-    public SpindexerHoming(SpindexerSubsystem spindexer){
-        while(!spindexer.getLimitSwitchState()) {
-            spindexer.setSpindexerPower(0.5);
-        }
-        spindexer.setSpindexerOffset(spindexer.getPosition());
-        spindexer.setSpindexerPower(0.0);
+    private final SpindexerSubsystem spindexer;
+    private boolean homed;
 
+    public SpindexerHoming(SpindexerSubsystem spindexer) {
+        this.spindexer = spindexer;
     }
+
+    @Override
+    public void initialize() {
+        homed = false;
+    }
+
+    @Override
+    public void execute() {
+        if (!spindexer.getLimitSwitchState()) {
+            spindexer.setSpindexerPower(0.5);
+        } else if (!homed) {
+            spindexer.setSpindexerOffset(spindexer.getPosition());
+            spindexer.setSpindexerPower(0.0);
+            homed = true;
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return homed;
+    }
+
 }
