be018d6c53560e9f873e4dc0bde5782bd78aa928
aaguy-hue <aaditmathur6@gmail.com>
2025-11-11 15:58:08 -0800

    adsjop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/EncoderTest.java
@@ -2,11 +2,13 @@ package org.firstinspires.ftc.teamcode.opmodes.testing;
 
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
 
+@TeleOp
 public class EncoderTest extends LinearOpMode {
 
     private RobotHardware hardware = new RobotHardware();
@@ -18,6 +20,7 @@ public class EncoderTest extends LinearOpMode {
         while(opModeIsActive())
         {
             telemetry.addData("spindexerencoderpos",String.valueOf(hardware.spindexerEncoder.getCurrentPosition()));
+            telemetry.update();
         }
     }
 }
