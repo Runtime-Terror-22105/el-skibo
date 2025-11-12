b7131a87db0664aff05e44b2c1441ce68bdf7793
aaguy-hue <aaditmathur6@gmail.com>
2025-11-11 17:51:15 -0800

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/ServoTest.java
@@ -13,22 +13,23 @@ public class ServoTest extends LinearOpMode {
     // targets
     public static double TURRET_YAW_LEFT_POS = 0.5;
     public static double TURRET_YAW_RIGHT_POS = 0.5;
-    public static double SHOOTER_PITCH_POS = 0.5;
-    public static double SPINDEXER_INTAKE_RAMP_1_POS = 0.5;
-    public static double SPINDEXER_INTAKE_RAMP_2_POS = 0.5;
-    public static double SPINDEXER_DIDDY_POS = 0.5;
-    public static double SPINDEXER_TRANSFER_RAMP_POS = 0.5;
-    public static double INTAKE_PITCH_1_POS = 0.5;
-    public static double INTAKE_PITCH_2_POS = 0.5;
-    public static double SPINDEXER_PTO_POS = 0.5;
+//    public static double SHOOTER_PITCH_POS = 0.5;
+//    public static double SPINDEXER_INTAKE_RAMP_1_POS = 0.5;
+//    public static double SPINDEXER_INTAKE_RAMP_2_POS = 0.5;
+//    public static double SPINDEXER_DIDDY_POS = 0.5;
+//    public static double SPINDEXER_TRANSFER_RAMP_POS = 0.5;
+//    public static double INTAKE_PITCH_1_POS = 0.5;
+//    public static double INTAKE_PITCH_2_POS = 0.5;
+//    public static double SPINDEXER_PTO_POS = 0.5;
 
-    public static boolean doTurret = false;
-    public static boolean doShooterPitch = false;
-    public static boolean doSpindexerIntakeRamps = false;
-    public static boolean doSpindexerDiddy = false;
-    public static boolean doSpindexerTransfer = false;
-    public static boolean doIntakePitch = false;
-    public static boolean doSpindexerPto = false;
+    public static boolean doTurretLeft = false;
+    public static boolean doTurretRight = false;
+//    public static boolean doShooterPitch = false;
+//    public static boolean doSpindexerIntakeRamps = false;
+//    public static boolean doSpindexerDiddy = false;
+//    public static boolean doSpindexerTransfer = false;
+//    public static boolean doIntakePitch = false;
+//    public static boolean doSpindexerPto = false;
 
     private final RobotHardware hardware = new RobotHardware();
 
@@ -44,30 +45,31 @@ public class ServoTest extends LinearOpMode {
                 }
             }
 
-            if (doTurret) {
+            if (doTurretLeft) {
                 if (hardware.turretYawLeft != null) hardware.turretYawLeft.setPosition(TURRET_YAW_LEFT_POS);
+            }
+            if (doTurretRight)
                 if (hardware.turretYawRight != null) hardware.turretYawRight.setPosition(TURRET_YAW_RIGHT_POS);
-            }
-            if (doShooterPitch && hardware.shooterPitch != null) {
-                hardware.shooterPitch.setPosition(SHOOTER_PITCH_POS);
-            }
-            if (doSpindexerIntakeRamps) {
-                if (hardware.spindexerIntakeRampServo1 != null) hardware.spindexerIntakeRampServo1.setPosition(SPINDEXER_INTAKE_RAMP_1_POS);
-                if (hardware.spindexerIntakeRampServo2 != null) hardware.spindexerIntakeRampServo2.setPosition(SPINDEXER_INTAKE_RAMP_2_POS);
-            }
-            if (doSpindexerDiddy && hardware.spindexerDiddyServo != null) {
-                hardware.spindexerDiddyServo.setPosition(SPINDEXER_DIDDY_POS);
-            }
-            if (doSpindexerTransfer && hardware.spindexerTransferRampServo != null) {
-                hardware.spindexerTransferRampServo.setPosition(SPINDEXER_TRANSFER_RAMP_POS);
-            }
-            if (doIntakePitch) {
-                if (hardware.intakePitch1 != null) hardware.intakePitch1.setPosition(INTAKE_PITCH_1_POS);
-                if (hardware.intakePitch2 != null) hardware.intakePitch2.setPosition(INTAKE_PITCH_2_POS);
-            }
-            if (doSpindexerPto && hardware.spindexerPTO != null) {
-                hardware.spindexerPTO.setPosition(SPINDEXER_PTO_POS);
-            }
+//            if (doShooterPitch && hardware.shooterPitch != null) {
+//                hardware.shooterPitch.setPosition(SHOOTER_PITCH_POS);
+//            }
+//            if (doSpindexerIntakeRamps) {
+//                if (hardware.spindexerIntakeRampServo1 != null) hardware.spindexerIntakeRampServo1.setPosition(SPINDEXER_INTAKE_RAMP_1_POS);
+//                if (hardware.spindexerIntakeRampServo2 != null) hardware.spindexerIntakeRampServo2.setPosition(SPINDEXER_INTAKE_RAMP_2_POS);
+//            }
+//            if (doSpindexerDiddy && hardware.spindexerDiddyServo != null) {
+//                hardware.spindexerDiddyServo.setPosition(SPINDEXER_DIDDY_POS);
+//            }
+//            if (doSpindexerTransfer && hardware.spindexerTransferRampServo != null) {
+//                hardware.spindexerTransferRampServo.setPosition(SPINDEXER_TRANSFER_RAMP_POS);
+//            }
+//            if (doIntakePitch) {
+//                if (hardware.intakePitch1 != null) hardware.intakePitch1.setPosition(INTAKE_PITCH_1_POS);
+//                if (hardware.intakePitch2 != null) hardware.intakePitch2.setPosition(INTAKE_PITCH_2_POS);
+//            }
+//            if (doSpindexerPto && hardware.spindexerPTO != null) {
+//                hardware.spindexerPTO.setPosition(SPINDEXER_PTO_POS);
+//            }
 
             hardware.write();
         }
