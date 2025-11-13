bbf15301aecad98c812f394665c69a24b016a1d8
Jonathan Liu <jonliu168@gmail.com>
2025-11-12 17:03:53 -0800

    i think aadit will enjoy doing this for me

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/ColorSensorTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes.testing;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
@@ -10,31 +11,37 @@ import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
 @TeleOp
 public class ColorSensorTest extends LinearOpMode {
     private final RobotHardware hardware = new RobotHardware();
+    private NormalizedColorSensor topSensor;
+    private NormalizedColorSensor leftSensor;
+    private NormalizedColorSensor rightSensor;
 
     @Override
     public void runOpMode() throws InterruptedException {
         hardware.init(hardwareMap, LynxModule.BulkCachingMode.AUTO);
+        topSensor = hardwareMap.get(NormalizedColorSensor.class,"topColor");
+        leftSensor = hardwareMap.get(NormalizedColorSensor.class,"leftSensor");
+        rightSensor = hardwareMap.get(NormalizedColorSensor.class,"rightSensor");
 
         waitForStart();
         while (opModeIsActive())
         {
-            NormalizedRGBA right = hardware.rightSensor.getNormalizedColors();
+            NormalizedRGBA right = rightSensor.getNormalizedColors();
             telemetry.addData("right sensor red", right.red * 255);
             telemetry.addData("right sensor green", right.green * 255);
             telemetry.addData("right sensor blue", right.blue * 255);
-            telemetry.addData("right sensor color", hardware.rightSensor.getGreenOrPurple());
+//            telemetry.addData("right sensor color", hardware.rightSensor.getGreenOrPurple());
 
-            NormalizedRGBA left = hardware.leftSensor.getNormalizedColors();
+            NormalizedRGBA left = leftSensor.getNormalizedColors();
             telemetry.addData("left sensor red", left.red * 255);
             telemetry.addData("left sensor green", left.green * 255);
             telemetry.addData("left sensor blue", left.blue * 255);
-            telemetry.addData("left sensor color", hardware.leftSensor.getGreenOrPurple());
+//            telemetry.addData("left sensor color", hardware.leftSensor.getGreenOrPurple());
 
-            NormalizedRGBA top = hardware.topSensor.getNormalizedColors();
+            NormalizedRGBA top = topSensor.getNormalizedColors();
             telemetry.addData("top sensor red", top.red * 255);
             telemetry.addData("top sensor green", top.green * 255);
             telemetry.addData("top sensor blue", top.blue * 255);
-            telemetry.addData("top sensor color", hardware.topSensor.getGreenOrPurple());
+//            telemetry.addData("top sensor color", hardware.topSensor.getGreenOrPurple());
             telemetry.update();
         }
     }
