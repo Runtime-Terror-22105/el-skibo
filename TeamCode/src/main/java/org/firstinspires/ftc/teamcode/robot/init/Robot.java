5e4a66a70766f2f3e02090913039c755aa102af5
aaguy-hue <aaditmathur6@gmail.com>
2025-11-12 17:39:16 -0800

    jadfiohap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/init/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/init/Robot.java
@@ -62,15 +62,15 @@ public class Robot {
         this.telemetry = new MultipleTelemetry(tele, dashboard.getTelemetry());
 
         // Initialize the localizer
-        if (hardware.pinpoint != null) {
-            this.pinpoint = new PinpointLocalizer(hardware.pinpoint/*, hardware.imu*/);
-            pinpoint.init(new PinpointLocalizer.Parameters(
-                    PINPOINT_X_OFFSET, PINPOINT_Y_OFFSET,
-                    TerrorPinpoint.GoBildaOdometryPods.goBILDA_4_BAR_POD,
-                    TerrorPinpoint.EncoderDirection.FORWARD, TerrorPinpoint.EncoderDirection.REVERSED
-            ));
-        }
-        localizer = new LocalizationSubsystem(new Pose2d(new Point(0.0,0.0), 0.0), this.hardware, this);
+//        if (hardware.pinpoint != null) {
+//            this.pinpoint = new PinpointLocalizer(hardware.pinpoint/*, hardware.imu*/);
+//            pinpoint.init(new PinpointLocalizer.Parameters(
+//                    PINPOINT_X_OFFSET, PINPOINT_Y_OFFSET,
+//                    TerrorPinpoint.GoBildaOdometryPods.goBILDA_4_BAR_POD,
+//                    TerrorPinpoint.EncoderDirection.FORWARD, TerrorPinpoint.EncoderDirection.REVERSED
+//            ));
+//        }
+//        localizer = new LocalizationSubsystem(new Pose2d(new Point(0.0,0.0), 0.0), this.hardware, this);
 
         // Initialize the drivetrain
         this.drivetrain = new MecanumDrivetrain(
@@ -79,10 +79,10 @@ public class Robot {
                 hardware.motorRearRight,
                 hardware.motorFrontRight
         );
-        this.shooter = new ShooterSubsystem(hardware);
+//        this.shooter = new ShooterSubsystem(hardware);
         this.spindexer = new SpindexerSubsystem(hardware);
-        this.intake = new IntakeSubsystem(hardware);
-        this.hang = new HangSubsystem(hardware);
+//        this.intake = new IntakeSubsystem(hardware);
+//        this.hang = new HangSubsystem(hardware);
 
         // Set up the camera
         if (hardware.fieldCamera != null) {
