6dded8ee0a971370611f389d131109ffab71ad96
aaguy-hue <aaditmathur6@gmail.com>
2025-11-16 10:48:36 -0800

    log rpm in motortest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/MotorTest.java
@@ -1,9 +1,15 @@
 package org.firstinspires.ftc.teamcode.opmodes.testing;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.robot.hardware.motors.TerrorMotorNormal;
+import org.firstinspires.ftc.teamcode.robot.hardware.sensors.TerrorEncoder;
 
 @TeleOp(name = "Motor Test", group = "Testing")
 @Config
@@ -15,9 +21,13 @@ public class MotorTest extends LinearOpMode {
     public static String motorName2 = "";
     public static double motorPower2 = 0D;
 
+    public static boolean showEncoderOutput = true;
+
 
     @Override
     public void runOpMode() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
         waitForStart();
 
         while (opModeIsActive()) {
@@ -27,6 +37,14 @@ public class MotorTest extends LinearOpMode {
 
             if (!motorName2.isEmpty()) {
                 hardwareMap.get(DcMotor.class, motorName2).setPower(motorPower2);
+            }
+
+            if (showEncoderOutput && !motorName.isEmpty()) {
+                TerrorEncoder shooterEncoder = new TerrorEncoder(new TerrorMotorNormal((DcMotorEx) hardwareMap.get(DcMotor.class, "shooterLeft"), 0.05, 1.0));
+                telemetry.addData("Current velocity (ticks/sec)", shooterEncoder.getVelocity());
+
+                // I'm pretty sure we're using this motor? https://www.gobilda.com/5202-series-yellow-jacket-planetary-gear-motor-5-2-1-ratio-1150-rpm-3-3-5v-encoder/
+                telemetry.addData("Current velocity (rpm)", shooterEncoder.getVelocity() * 60 / 145.1); // 145.1 ticks per revolution
             }
         }
     }
