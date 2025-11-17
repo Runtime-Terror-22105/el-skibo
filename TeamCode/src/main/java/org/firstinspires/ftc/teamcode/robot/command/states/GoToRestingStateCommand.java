81d2fcc20b57744b35af22ec7eac494eae997e54
aaguy-hue <aaditmathur6@gmail.com>
2025-11-16 21:59:35 -0800

    closes #99

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/states/GoToRestingStateCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/states/GoToRestingStateCommand.java
@@ -6,6 +6,9 @@ import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakeDownCommand;
 import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakeSpeedCommand;
+import org.firstinspires.ftc.teamcode.robot.command.spindexer.SetSpindexerPoleActive;
+import org.firstinspires.ftc.teamcode.robot.command.spindexer.SetSpindexerRampActive;
+import org.firstinspires.ftc.teamcode.robot.command.spindexer.SetSpindexerWallActive;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotState;
 import org.firstinspires.ftc.teamcode.robot.command.shooter.SetShooterManualAimCommand;
@@ -16,7 +19,10 @@ public class GoToRestingStateCommand extends SequentialCommandGroup {
                 new InstantCommand(() -> robot.robotState = RobotState.RESTING),
                 new SetShooterManualAimCommand(robot.shooter, 0.0, 45.0, 0.0),
                 new SetIntakeDownCommand(robot.intake, false),
-                new SetIntakeSpeedCommand(robot.intake, 0.0)
+                new SetIntakeSpeedCommand(robot.intake, 0.0),
+                new SetSpindexerPoleActive(robot.spindexer, false),
+                new SetSpindexerRampActive(robot.spindexer, false)
+//                new SetSpindexerWallActive(robot.spindexer, true)
         ));
     }
 }
