44a808e92b9af8913b2b1a0c65e55c00790fe968
Serpintini <136918763+Serpintini@users.noreply.github.com>
2025-11-18 08:33:27 -0800

    autoaim tunung

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/ShooterAimingTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/ShooterAimingTuner.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes.tuning;
 
+import android.util.Log;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -15,9 +17,10 @@ import org.firstinspires.ftc.teamcode.robot.command.spindexer.TransferCommand;
 import org.firstinspires.ftc.teamcode.robot.command.states.GoToRestingStateCommand;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
+import org.firstinspires.ftc.teamcode.robot.subsystems.ShotType;
 
 @Config
-@TeleOp(name="Shooter Tuner", group="Tuning")
+@TeleOp(name="Shooter Aim Tuner", group="Tuning")
 public class ShooterAimingTuner extends LinearOpMode {
 
     private final RobotHardware hardware = new RobotHardware();
@@ -25,8 +28,8 @@ public class ShooterAimingTuner extends LinearOpMode {
 
     public static double GOAL_RPM = 0.0;
 
-    public static double DIST_TO_GOAL_X = -1; // in mm
-    public static double DIST_TO_GOAL_Y = -1; // in mm
+    public static double DIST_TO_GOAL_X = 81; // in mm
+    public static double DIST_TO_GOAL_Y = 26; // in mm
 
     public static double DIST_TO_GOAL = -1; // in mm
     public static double ROBOT_ANGLE_TO_GOAL = -1; // in degrees
@@ -50,40 +53,45 @@ public class ShooterAimingTuner extends LinearOpMode {
                 hub.clearBulkCache();
             }
 
-            Pose2d robotPose;
-            if (DIST_TO_GOAL_X != -1 && DIST_TO_GOAL_Y != -1) {
-                robotPose = new Pose2d(DIST_TO_GOAL_X, DIST_TO_GOAL_Y, Math.atan2(DIST_TO_GOAL_Y, DIST_TO_GOAL_X));
-            } else if (DIST_TO_GOAL != -1) {
-                double angleRad;
-                if (ROBOT_ANGLE_TO_GOAL != -1) {
-                    angleRad = Math.toRadians(ROBOT_ANGLE_TO_GOAL);
-                } else {
-                    // note: you might want to test out whether the internal imu works properly separately using InternalIMUTest
-                    angleRad = hardwareMap.get(IMU.class, "imu").getRobotYawPitchRollAngles().getYaw();
-                }
-                double x = DIST_TO_GOAL * Math.cos(angleRad);
-                double y = DIST_TO_GOAL * Math.sin(angleRad);
-                robotPose = new Pose2d(x, y, angleRad);
-            } else {
-                robotPose = new Pose2d(0, 0, 0);
-            }
+//            Pose2d robotPose;
+//            if (DIST_TO_GOAL_X != -1 && DIST_TO_GOAL_Y != -1) {
+//                robotPose = new Pose2d(DIST_TO_GOAL_X, DIST_TO_GOAL_Y, Math.atan2(DIST_TO_GOAL_Y, DIST_TO_GOAL_X));
+//            } else if (DIST_TO_GOAL != -1) {
+//                double angleRad;
+//                if (ROBOT_ANGLE_TO_GOAL != -1) {
+//                    angleRad = Math.toRadians(ROBOT_ANGLE_TO_GOAL);
+//                } else {
+//                    // note: you might want to test out whether the internal imu works properly separately using InternalIMUTest
+//                    angleRad = hardwareMap.get(IMU.class, "imu").getRobotYawPitchRollAngles().getYaw();
+//                }
+//                double x = DIST_TO_GOAL * Math.cos(angleRad);
+//                double y = DIST_TO_GOAL * Math.sin(angleRad);
+//                robotPose = new Pose2d(x, y, angleRad);
+//            } else {
+//                robotPose = new Pose2d(0, 0, 0);
+//            }
+            Pose2d robotPose = new Pose2d(DIST_TO_GOAL_X, DIST_TO_GOAL_Y, Math.atan2(DIST_TO_GOAL_Y, DIST_TO_GOAL_X));
+            Pose2d goalPos = new Pose2d(0, 0, 0);
 
-
-            robot.shooter.doAutoShoot(robotPose);
+            robot.shooter.doMath(robotPose, goalPos, ShotType.Arc, 60.0);
+            robot.shooter.manualAim(robot.shooter.goalVelocity, robot.shooter.goalPitch, 0);
 
             if (gamepad1.rightBumperWasPressed()) {
                 CommandScheduler.getInstance().schedule(new TransferCommand(robot));
             }
 
-            robot.shooter.setSpeed(GOAL_RPM);
+            //robot.shooter.setSpeed(GOAL_RPM);
 
             CommandScheduler.getInstance().run();
 
             hardware.write();
 
 
-            robot.telemetry.addData("Goal Velocity", GOAL_RPM);
-            robot.telemetry.addData("Current velocity",robot.shooter.getVelocityRpm());
+            robot.telemetry.addData("Goal Yaw", robot.shooter.goalYaw);
+            robot.telemetry.addData("Goal Velocity in/sec", robot.shooter.goalVelocity);
+            robot.telemetry.addData("Goal Velocity rpm", robot.shooter.velToRPM(robot.shooter.goalVelocity));
+            robot.telemetry.addData("Goal Pitch", robot.shooter.goalPitch);
+            robot.telemetry.addData("Current velocity rpm",robot.shooter.getVelocityRpm());
             robot.telemetry.update();
 
         }
