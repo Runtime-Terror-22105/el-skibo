9ed8811d029a89113177170dade8c5c0b4804c82
Anonymous Member 1 <anonymous@example.com>
2025-11-19 17:28:50 -0800

    make intake pitch an enum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/Intaketesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/Intaketesting.java
@@ -6,16 +6,12 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
 
-import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakeDownCommand;
-import org.firstinspires.ftc.teamcode.robot.command.shooter.SpindexerHoming;
-import org.firstinspires.ftc.teamcode.robot.hardware.motors.TerrorMotorNormal;
-import org.firstinspires.ftc.teamcode.robot.hardware.sensors.TerrorEncoder;
+import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakePitchCommand;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
+import org.firstinspires.ftc.teamcode.robot.subsystems.intake.IntakePitch;
 
 @TeleOp(name = "Intake Test", group = "Testing")
 @Config
@@ -34,7 +30,7 @@ public class Intaketesting extends LinearOpMode {
 
         robot.init(hardware, telemetry);
 
-        SetIntakeDownCommand intakecommand= new SetIntakeDownCommand(robot.intake, true);
+        SetIntakePitchCommand intakecommand= new SetIntakePitchCommand(robot.intake, IntakePitch.DOWN);
         waitForStart();
         CommandScheduler.getInstance().schedule(intakecommand);
         CommandScheduler.getInstance().run();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/IntakeSubsystem.java
@@ -4,48 +4,39 @@ import com.acmerobotics.dashboard.config.Config;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
+import org.firstinspires.ftc.teamcode.robot.subsystems.intake.IntakePitch;
 
 @Config
 public class IntakeSubsystem extends SubsystemBase {
     private final RobotHardware hardware;
 
-    public static double PITCH_DOWN_LEFT = 0.91; //servo pos
-    public static double PITCH_UP_LEFT = 0.41;
-    public static double PITCH_DOWN_RIGHT = 0.0;
-    public static double PITCH_UP_RIGHT = 0.5;
+    public static double DOWN_LEFT = 0.91; //servo pos
+    public static double DOWN_RIGHT = 0.0;
+    public static double UP_LEFT = 0.41;
+    public static double UP_RIGHT = 0.5;
+
+    public static double MIN_LEFT = 0.41;
+    public static double MAX_LEFT = 0.91; //servo pos
+    public static double MIN_RIGHT = 0.0;
+    public static double MAX_RIGHT = 0.5;
 
     public static double DEFAULT_SPEED = 0.9;
 
-    private double targetPitchLeft;
-    private double targetPitchRight;
     private double targetSpeed;
 
+    private IntakePitch pitch = IntakePitch.DOWN;
 
     public IntakeSubsystem(RobotHardware hardware) {
         this.hardware = hardware;
-        this.targetPitchLeft = PITCH_UP_LEFT;
-        this.targetPitchRight = PITCH_UP_RIGHT;
         this.targetSpeed = 0;
     }
 
-    public void putDown(){
-        this.targetPitchLeft = PITCH_DOWN_LEFT;
-        this.targetPitchRight = PITCH_DOWN_RIGHT;
+    public void setPitch(IntakePitch pitch) {
+        this.pitch = pitch;
     }
 
-    public void putUp(){
-        this.targetPitchLeft = PITCH_UP_LEFT;
-        this.targetPitchRight = PITCH_UP_RIGHT;
-    }
-
-    public boolean isUp() {
-        return this.targetPitchLeft == PITCH_UP_LEFT && this.targetPitchRight == PITCH_UP_RIGHT;
-    }
-
-    public void setPitch(double pitch) {
-        // TODO: this method should map each of the ranges to 0-1
-//        this.targetPitchLeft = pitch;
-//        this.targetPitchRight = 0.5 - (0.5 - pitch);
+    public IntakePitch getPitch() {
+        return pitch;
     }
 
     public void turnOn(){
@@ -60,10 +51,6 @@ public class IntakeSubsystem extends SubsystemBase {
         this.targetSpeed = speed;
     }
 
-    public double getPitch(){
-        return this.targetPitchLeft;
-    }
-
     public double getSpeed(){
         return this.targetSpeed;
     }
@@ -71,7 +58,7 @@ public class IntakeSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         hardware.intake.setPower(this.targetSpeed);
-        hardware.intakePitchLeft.setPosition(targetPitchLeft);
-        hardware.intakePitchRight.setPosition(targetPitchRight);
+        hardware.intakePitchLeft.setPosition(Math.max(MIN_LEFT, Math.max(MAX_LEFT, pitch.left.get())));
+        hardware.intakePitchRight.setPosition(Math.max(MIN_RIGHT, Math.max(MAX_RIGHT, pitch.right.get())));
     }
 }
