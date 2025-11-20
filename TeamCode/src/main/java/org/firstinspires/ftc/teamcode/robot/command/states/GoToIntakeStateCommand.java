cc80c748b4e8b09c0da933acaba7ab7a9df87de9
Landon <53448214+lunbun@users.noreply.github.com>
2025-11-19 17:28:50 -0800

    make intake pitch an enum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/states/GoToIntakeStateCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/states/GoToIntakeStateCommand.java
@@ -4,19 +4,20 @@ import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 
-import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakeDownCommand;
+import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakePitchCommand;
 import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakeSpeedCommand;
 import org.firstinspires.ftc.teamcode.robot.command.spindexer.SetSpindexerWallDown;
 import org.firstinspires.ftc.teamcode.robot.command.spindexer.TransferCommand;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotState;
+import org.firstinspires.ftc.teamcode.robot.subsystems.intake.IntakePitch;
 import org.firstinspires.ftc.teamcode.robot.subsystems.IntakeSubsystem;
 
 public class GoToIntakeStateCommand extends SequentialCommandGroup {
     public GoToIntakeStateCommand(Robot robot, TransferCommand transfer) {
         super(new ParallelCommandGroup(
                 new InstantCommand(() -> robot.robotState = RobotState.INTAKING),
-                new SetIntakeDownCommand(robot.intake, true),
+                new SetIntakePitchCommand(robot.intake, IntakePitch.DOWN),
                 new SetIntakeSpeedCommand(robot.intake, IntakeSubsystem.DEFAULT_SPEED),
                 new SetSpindexerWallDown(robot.spindexer, true)
 //              I don't think this should be here:  new InstantCommand(() -> transfer.setupTransfer())
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/states/GoToRestingStateCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/states/GoToRestingStateCommand.java
@@ -4,20 +4,21 @@ import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 
-import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakeDownCommand;
+import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakePitchCommand;
 import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakeSpeedCommand;
 import org.firstinspires.ftc.teamcode.robot.command.spindexer.SetSpindexerPoleActive;
 import org.firstinspires.ftc.teamcode.robot.command.spindexer.SetSpindexerRampActive;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotState;
 import org.firstinspires.ftc.teamcode.robot.command.shooter.SetShooterManualAimCommand;
+import org.firstinspires.ftc.teamcode.robot.subsystems.intake.IntakePitch;
 
 public class GoToRestingStateCommand extends SequentialCommandGroup {
     public GoToRestingStateCommand(Robot robot) {
         super(new ParallelCommandGroup(
                 new InstantCommand(() -> robot.robotState = RobotState.RESTING),
                 new SetShooterManualAimCommand(robot.shooter, 0.0, 45.0, 0.0),
-                new SetIntakeDownCommand(robot.intake, false),
+                new SetIntakePitchCommand(robot.intake, IntakePitch.UP),
                 new SetIntakeSpeedCommand(robot.intake, 0.0),
                 new SetSpindexerPoleActive(robot.spindexer, false),
                 new SetSpindexerRampActive(robot.spindexer, false)
