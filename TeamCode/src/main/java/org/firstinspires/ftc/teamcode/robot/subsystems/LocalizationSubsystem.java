67fe656ff7b00e86b7228778a8e30f4f3fded1ed
Anonymous Member 1 <anonymous@example.com>
2025-11-20 21:00:03 -0800

    use pedro teleop follower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/LocalizationSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/LocalizationSubsystem.java
@@ -5,14 +5,12 @@ import android.util.Log;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 import org.firstinspires.ftc.teamcode.math.Pose2d;
-import org.firstinspires.ftc.teamcode.robot.drive.localizer.PinpointLocalizer;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
 
 public class LocalizationSubsystem extends SubsystemBase {
 
     public Pose2d currentPosition;
-    public PinpointLocalizer pinpointLocalizer;
 
     private final RobotHardware hardware;
 
