069965368e3ef0fca1cecc361be13bc74d179b71
Landon <53448214+lunbun@users.noreply.github.com>
2025-11-22 15:55:59 -0800

    testing auto more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team.java
@@ -1,20 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.pedropathing.geometry.Pose;
-
 import org.firstinspires.ftc.teamcode.math.Pose2d;
 
 import java.util.function.Supplier;
 
 public enum Team {
     BLUE(() -> FieldConstants.BLUE_GOAL_POS, () -> FieldConstants.BLUE_START_POS_AUTO),
-    RED(() -> FieldConstants.RED_GOAL_POS,
-            () -> FieldConstants.RED_START_POS_AUTO);
+    RED(() -> FieldConstants.RED_GOAL_POS, () -> FieldConstants.RED_START_POS_AUTO);
 
     private final Supplier<Pose2d> goalPos;
-    private final Supplier<Pose> startPosAuto;
+    private final Supplier<Pose2d> startPosAuto;
 
-    Team(Supplier<Pose2d> goalPos, Supplier<Pose> startPosAuto) {
+    Team(Supplier<Pose2d> goalPos, Supplier<Pose2d> startPosAuto) {
         this.goalPos = goalPos;
         this.startPosAuto = startPosAuto;
     }
@@ -23,7 +20,7 @@ public enum Team {
         return goalPos.get();
     }
 
-    public Pose getStartPosAuto() {
+    public Pose2d getStartPosAuto() {
         return startPosAuto.get();
     }
 }
