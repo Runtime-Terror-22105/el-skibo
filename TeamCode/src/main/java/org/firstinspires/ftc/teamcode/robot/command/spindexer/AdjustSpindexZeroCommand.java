452b6d12955db3a96ee893b213e0807b3737799e
aaguy-hue <aaditmathur6@gmail.com>
2025-11-22 22:10:19 -0800

    aijfdpa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/spindexer/AdjustSpindexZeroCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/spindexer/AdjustSpindexZeroCommand.java
@@ -1,13 +1,15 @@
 package org.firstinspires.ftc.teamcode.robot.command.spindexer;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.seattlesolvers.solverslib.command.CommandBase;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.subsystems.SpindexerSubsystem;
 
+@Config
 public class AdjustSpindexZeroCommand extends InstantCommand {
-    public static double INCREASE_AMOUNT_DEGREES = 5;
+    public static double INCREASE_AMOUNT_DEGREES = 2;
 
     public AdjustSpindexZeroCommand(Robot robot, boolean increase) {
         super(() -> {
