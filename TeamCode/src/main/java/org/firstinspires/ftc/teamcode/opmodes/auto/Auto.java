558acc38918c32df8aa31d90048ab346e043ddf0
Landon <53448214+lunbun@users.noreply.github.com>
2025-11-22 18:15:07 -0800

    working auto (kinda)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Auto.java
@@ -44,18 +44,18 @@ public abstract class Auto extends LinearOpMode {
     public static Pose2d PUSH_GATE_POSE = new Pose2d(23, 72.827, Math.toRadians(180));
     public static Pose2d SHOOT_POSE = new Pose2d(60, 87.449, Math.toRadians(315));
 
-    public static Pose2d PREPARE_INTAKE_2_POSE = new Pose2d(PREPARE_INTAKE_1_POSE.x, 65, Math.toRadians(180));
-    public static Pose2d INTAKE_2_POSE = new Pose2d(INTAKE_1_POSE.x, 65, Math.toRadians(180));
+    public static Pose2d PREPARE_INTAKE_2_POSE = new Pose2d(PREPARE_INTAKE_1_POSE.x, 63, Math.toRadians(180));
+    public static Pose2d INTAKE_2_POSE = new Pose2d(INTAKE_1_POSE.x, 63, Math.toRadians(180));
 
     public static Pose2d PREPARE_INTAKE_3_POSE = new Pose2d(PREPARE_INTAKE_1_POSE.x, 40, Math.toRadians(180));
     public static Pose2d INTAKE_3_POSE = new Pose2d(INTAKE_1_POSE.x, 40, Math.toRadians(180));
 
     public static Pose2d PARK_POSE = new Pose2d(52.282, 120.575, Math.toRadians(315));
 
-    public static int PRE_INTAKE_DELAY = 200;
-    public static int INTAKE_DELAY = 200;
-    public static int PRE_SHOOT_DELAY = 200;
-    public static int SHOOT_DELAY = 200;
+    public static int PRE_INTAKE_DELAY = 0;
+    public static int INTAKE_DELAY = 0;
+    public static int PRE_SHOOT_DELAY = 0;
+    public static int SHOOT_DELAY = 0;
 
     private final RobotHardware hardware = new RobotHardware();
     private final Robot robot = new Robot();
