e234935953be1e004c1a449976420bc625ad13c2
aaguy-hue <aaditmathur6@gmail.com>
2025-11-23 09:37:26 -0800

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/shooter/ShooterLookupTable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/shooter/ShooterLookupTable.java
@@ -1,9 +1,17 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems.shooter;
 
+import android.util.Pair;
+
+import com.acmerobotics.dashboard.config.Config;
 import com.seattlesolvers.solverslib.util.InterpLUT;
 
 import org.firstinspires.ftc.teamcode.robot.subsystems.ShooterSubsystem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Config
 public class ShooterLookupTable {
     private static final double MIN_DIST = 35.01;
     private static final double MAX_DIST = 69.967;
@@ -14,28 +22,51 @@ public class ShooterLookupTable {
     private static final double FAR_HOOD = 1.0; // rad
 
     private static final InterpLUT SHORT_VELOCITY_LUT = new InterpLUT(); // in/s
-    private static final InterpLUT FAR_VELOCITY_LUT = new InterpLUT(); // in/s
+    private static InterpLUT FAR_VELOCITY_LUT = new InterpLUT(); // in/s
+
+    public static class LookupValue {
+        public double distance;
+        public double speed;
+
+        public LookupValue(double d, double s) {
+            this.distance = d;
+            this.speed = s;
+        }
+    }
+
+    public static LookupValue[] DATA_POINTS = new LookupValue[]
+            {
+                    new LookupValue(35, 450),
+                    new LookupValue(45, 470),
+                    new LookupValue(55, 490),
+                    new LookupValue(60, 500),
+                    new LookupValue(70, 530)
+            };
 
     static {
-        FAR_VELOCITY_LUT.add(35, 430);
-        FAR_VELOCITY_LUT.add(45, 450);
-        FAR_VELOCITY_LUT.add(55, 500);
-        FAR_VELOCITY_LUT.add(60, 510);
-        FAR_VELOCITY_LUT.add(70, 545);
-
-//        SHORT_VELOCITY_LUT.createLUT();
-        FAR_VELOCITY_LUT.createLUT();
+//        for (int i = 0; i < DATA_POINTS.size(); i++) {
+//            FAR_VELOCITY_LUT.add(DATA_POINTS.get(i).first, DATA_POINTS.get(i).second);
+//        }
+//        FAR_VELOCITY_LUT.createLUT();
     }
 
     public static ShooterSubsystem.ShooterValues get(double distanceToGoalIn) {
+        // todo: temporarily putting this here so we can dashboard
+        FAR_VELOCITY_LUT = new InterpLUT();
+        for (LookupValue dataPoint : DATA_POINTS) {
+            FAR_VELOCITY_LUT.add(dataPoint.distance, dataPoint.speed);
+        }
+        FAR_VELOCITY_LUT.createLUT();
+
+
         distanceToGoalIn = Math.max(MIN_DIST, Math.min(MAX_DIST, distanceToGoalIn));
         double velocity, hood;
 //        if (distanceToGoalIn < CUTOFF) {
 //            velocity = SHORT_VELOCITY_LUT.get(distanceToGoalIn);
 //            hood = SHORT_HOOD;
 //        } else {
-            velocity = FAR_VELOCITY_LUT.get(distanceToGoalIn);
-            hood = FAR_HOOD;
+        velocity = FAR_VELOCITY_LUT.get(distanceToGoalIn);
+        hood = FAR_HOOD;
 //        }
         return new ShooterSubsystem.ShooterValues(velocity, hood);
     }
