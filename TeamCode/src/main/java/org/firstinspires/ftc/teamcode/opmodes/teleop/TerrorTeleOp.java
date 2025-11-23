295003bb3f3f7630dffcb87d058622ae3d8c5b87
Anonymous Member 1 <anonymous@example.com>
2025-11-22 16:32:24 -0800

    auto pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TerrorTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TerrorTeleOp.java
@@ -27,7 +27,7 @@ import com.seattlesolvers.solverslib.command.button.Trigger;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
-import org.firstinspires.ftc.teamcode.robot.command.spindexer.TransferCommand;
+import org.firstinspires.ftc.teamcode.robot.command.spindexer.PrepareShootCommand;
 import org.firstinspires.ftc.teamcode.robot.command.states.GoToIntakeStateCommand;
 import org.firstinspires.ftc.teamcode.robot.command.states.GoToRestingStateCommand;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
@@ -127,7 +127,7 @@ public abstract class TerrorTeleOp extends LinearOpMode {
         reverseIntakeButton.whenInactive(new SetIntakeSpeedCommand(robot.intake, 0.0));
 
         threeBallsAreInside.whenActive(new ConditionalCommand(
-                new TransferCommand(robot),
+                new PrepareShootCommand(robot),
                 new InstantCommand(() -> {} ),
                 () -> robot.robotState == RESTING || robot.robotState == INTAKING
         ));
@@ -136,7 +136,7 @@ public abstract class TerrorTeleOp extends LinearOpMode {
                 new ConditionalCommand( // if we already did the transfer, just shoot immediately
                         new ShootThreeBallsCommand(robot),
                         new SequentialCommandGroup(
-                                new TransferCommand(robot),
+                                new PrepareShootCommand(robot),
                                 new ShootThreeBallsCommand(robot)
                         ),
                         () -> robot.robotState == READY_TO_SHOOT
@@ -146,7 +146,7 @@ public abstract class TerrorTeleOp extends LinearOpMode {
         ));
 
         shoot1button.whenPressed(new ConditionalCommand(
-                new TransferCommand(robot),
+                new PrepareShootCommand(robot),
                 new InstantCommand(() -> {} ),
                 () -> robot.robotState != SHOOTING
         ));
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/spindexer/PrepareShootCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/spindexer/PrepareShootCommand.java
@@ -5,29 +5,31 @@ import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import com.seattlesolvers.solverslib.command.WaitCommand;
 
+import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakePitchCommand;
 import org.firstinspires.ftc.teamcode.robot.command.intake.SetIntakeSpeedCommand;
 import org.firstinspires.ftc.teamcode.robot.command.shooter.SetShooterRPMCommand;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotState;
-import org.firstinspires.ftc.teamcode.robot.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.robot.subsystems.intake.IntakePitch;
 
 @Config
-public class TransferCommand extends SequentialCommandGroup {
+public class PrepareShootCommand extends SequentialCommandGroup {
     public static double SHOOTER_RPM = 3500;
     public static double PRE_YAW_ANGLE = 30.0;  // degrees
     public static int PRE_YAW_DELAY = 250;  // milliseconds
     public static int RAMP_DELAY = 500;  // milliseconds
-    public static double SPINDEX_ROTATIONS = -4.5;  // revolutions, negative bc clockwise
 
     private final Robot robot;
 
-    public TransferCommand(Robot robot) {
+    public PrepareShootCommand(Robot robot) {
         this(robot, SHOOTER_RPM);
     }
 
-    public TransferCommand(Robot robot, double rpm) {
+    public PrepareShootCommand(Robot robot, double rpm) {
         super(
                 // Phase 1 and 2: ???
+                new SetIntakePitchCommand(robot.intake, IntakePitch.DOWN),
+                new SetIntakeSpeedCommand(robot.intake, 0),
                 new SetSpindexerWallDown(robot.spindexer, false),
                 new SetSpindexerPoleActive(robot.spindexer, true),
                 new SetShooterRPMCommand(robot.shooter, rpm),
@@ -41,11 +43,7 @@ public class TransferCommand extends SequentialCommandGroup {
 
                 // Phase 4: drop down ramp and start intake
                 new SetSpindexerRampActive(robot.spindexer, true),
-                new SetIntakeSpeedCommand(robot.intake, IntakeSubsystem.DEFAULT_SPEED),
                 new WaitCommand(RAMP_DELAY), // todo: adjust this delay based on how long it takes for ramp to drop
-
-                // Phase 5: transfer balls
-                new ChangeSpindexerYawCommand(robot.spindexer, SPINDEX_ROTATIONS*2*Math.PI),
 
                 // set to full state
                 new InstantCommand(() -> robot.robotState = RobotState.READY_TO_SHOOT)
