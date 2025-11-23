cfee6b08a7ee51b111331e4de2e6e80ed0a78c6e
Anonymous Member 1 <anonymous@example.com>
2025-11-22 23:12:38 -0800

    add red teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/RedRCTele.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Team;
+
+@TeleOp(name = "Red RC TeleOp", group = "TeleOp")
+@Config
+public class RedRCTele extends TerrorTeleOp {
+
+    public RedRCTele(){
+        super(Team.RED);
+    }
+
+    @Override
+    public void runOpMode() {
+        super.runOpMode();
+    }
+}
