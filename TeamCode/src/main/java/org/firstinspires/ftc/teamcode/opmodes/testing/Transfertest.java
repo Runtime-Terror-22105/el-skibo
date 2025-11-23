1cc032d5ed0369e5ac75c7145547937bc6cc531d
Anonymous Member 1 <anonymous@example.com>
2025-11-22 17:41:53 -0800

    Update Transfertest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/Transfertest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/Transfertest.java
@@ -1,27 +1,51 @@
 package org.firstinspires.ftc.teamcode.opmodes.testing;
 
 import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.seattlesolvers.solverslib.command.CommandScheduler;
 
+import org.firstinspires.ftc.teamcode.robot.command.shooter.SpindexerHoming;
+import org.firstinspires.ftc.teamcode.robot.command.spindexer.TransferCommand;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
 
-@TeleOp(name="Transfer Test", group="Testing")
-public class Transfertest extends LinearOpMode {
-    private RobotHardware hardware = new RobotHardware();
+@TeleOp
+public class TransferTest extends LinearOpMode {
+    private final RobotHardware hardware = new RobotHardware();
     private final Robot robot = new Robot();
-
     @Override
-    public void runOpMode() throws InterruptedException {
-        hardware.init(hardwareMap, LynxModule.BulkCachingMode.AUTO);
+    public void runOpMode() {
+        hardware.init(hardwareMap, LynxModule.BulkCachingMode.MANUAL);
         robot.init(hardware, telemetry);
-
-
         waitForStart();
-        while(opModeIsActive())
-        {
 
+        TransferCommand transfer = new TransferCommand(robot.spindexer);
+        while (opModeIsActive()) {
+            // Manually clear the bulk read cache. Deleting this would be catastrophic b/c stale
+            // vals would be used.
+            for (LynxModule hub : hardware.allHubs) {
+                hub.clearBulkCache();
+            }
+
+            if (gamepad1.aWasPressed()) {
+                transfer.phase1();
+            }
+            else if (gamepad1.bWasPressed()) {
+                transfer.phase2();
+            }
+            else if (gamepad1.yWasPressed()) {
+                transfer.phase3();
+            }
+            else if (gamepad1.xWasPressed()) {
+                transfer.phase4();
+            }
+            else if (gamepad1.leftBumperWasPressed()) {
+                transfer.phase5();
+            }
+            hardware.write();
         }
+
     }
 }
