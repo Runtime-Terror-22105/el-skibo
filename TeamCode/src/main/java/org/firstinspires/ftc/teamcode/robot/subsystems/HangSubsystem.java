1782c5372ade9473393109d48f1020d17961b3ca
aaguy-hue <aaditmathur6@gmail.com>
2025-11-23 15:09:15 -0800

    aeuproqewthjoboaher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/HangSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/HangSubsystem.java
@@ -30,24 +30,24 @@ public class HangSubsystem extends SubsystemBase {
     }
 
     public void setPTOState(boolean state) {
-        ptoEngaged = state;
-        hardware.spindexerPTO.setPosition(ptoEngaged ? PTO_ENGAGED_POSITION : PTO_DISENGAGED_POSITION);
+//        ptoEngaged = state;
+//        hardware.spindexerPTO.setPosition(ptoEngaged ? PTO_ENGAGED_POSITION : PTO_DISENGAGED_POSITION);
     }
 
     @Override
     public void periodic() {
-        if(!ptoEngaged) return;
-
+//        if(!ptoEngaged) return;
+//
 //        double roll = hardware.imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.DEGREES);
 //        Log.i("HangSubsystem", "Robot Roll: " + roll + " deg");
 
 //        if (roll < HANG_ANGLE_STOP_DEGREES) hardware.spindexerRotate.setPower(HANG_SPINDEXER_POWER);
 //        else hardware.spindexerRotate.setPower(0);
 
-        if (hangIsHeld) {
-            hardware.spindexerRotate.setPower(HANG_SPINDEXER_POWER);
-        } else {
-            hardware.spindexerRotate.setPower(0);
-        }
+//        if (hangIsHeld) {
+//            hardware.spindexerRotate.setPower(HANG_SPINDEXER_POWER);
+//        } else {
+//            hardware.spindexerRotate.setPower(0);
+//        }
     }
 }
