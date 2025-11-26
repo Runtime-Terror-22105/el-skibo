1e3aaa8c88e9276ef88a6b1d750b18fddd5b5ef1
Serpintini <136918763+Serpintini@users.noreply.github.com>
2025-11-25 19:45:55 -0800

    reorganize spindexer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
@@ -15,16 +15,13 @@ import org.firstinspires.ftc.teamcode.robot.subsystems.vision.CameraSubsystem;
 
 @Config
 public class SpindexerSubsystem extends SubsystemBase {
-    private enum COLOR {
-        GREEN, PURPLE
-    }
 
     private final RobotHardware hardware;
     private final Robot robot;
 
     private double homedSpindexerOffset = 0;
 
-    public static double SPINDEXER_OFFSET_2 = 0;
+    public static double SPINDEXER_OFFSET = 0;
 
     public static double TICKS_PER_REVOLUTION = ((1D + (46D / 11D)) * 28D) * 5.6D;
 
@@ -35,14 +32,6 @@ public class SpindexerSubsystem extends SubsystemBase {
 
     public static double SHOOTER_RAMP_ACTIVE = 0.33;
     public static double SHOOTER_RAMP_DEACTIVE = 0.0;
-    public static double RESTING_SPINDEX_POS = 0.0;
-
-    public static double TRANSFER_POWER = -1.0;
-
-    public double SHOOTER_INTAKE_SPEED = 0.0; // this is the speed where the shooter melonbotic servo intakes the balls
-
-    public static double SHOOTER_INTAKING_SPEED = 1.0;
-    public static double SHOOT_ONE_ROTATION = -(2D / 3D) * Math.PI;
 
     public double intakeWallPosition1 = INTAKE_WALL_1_UP;
     public double intakeWallPosition2 = INTAKE_WALL_2_UP;
@@ -57,13 +46,8 @@ public class SpindexerSubsystem extends SubsystemBase {
     public double spindexerPower = 0.0;
     public TerrorColorSensor[] sensors;
 
-    public boolean transferActive = false;
-
-    public static double LEFT_POSITION = (4D / 3D) * Math.PI;
-    public static double RIGHT_POSITION = (2D / 3D) * Math.PI;
-    public static double BACK_POSITION = 0.0;
     public static double READY_POSITION = (1D / 6D) * Math.PI; //position for the first ball as the ramp goes down
-    double[] yawOffsets = {0, (2.0 / 3) * Math.PI, -((2.0 / 3) * Math.PI)}; // todo: this is currently duplicate, make it so it just uses the above 3 variables
+    double[] yawOffsets = {0, (2.0 / 3) * Math.PI, -((2.0 / 3) * Math.PI)};
 
     public static PidfController.PidfCoefficients turningPidCoefficients =
             new PidfController.PidfCoefficients(0.011, 0, 0.00001, 0, 0);
@@ -77,7 +61,6 @@ public class SpindexerSubsystem extends SubsystemBase {
         this.robot = robot;
         this.hardware = hardware;
         this.sensors = new TerrorColorSensor[]{hardware.rightSensor, hardware.topSensor, hardware.leftSensor};
-
         this.desiredAngle = getPosition();
         goToAngle120(0);
         this.yawPid.setTolerance(radiansToTicks(yawPidTolerance));
@@ -90,6 +73,14 @@ public class SpindexerSubsystem extends SubsystemBase {
 
     public static double radiansToTicks(double radians) {
         return (radians / (2.0 * Math.PI)) * TICKS_PER_REVOLUTION;
+    }
+
+    public double getPositionTicks() {
+        return hardware.spindexerEncoder.getCurrentPosition() - this.homedSpindexerOffset - SPINDEXER_OFFSET;
+    }
+
+    public double getPosition() {
+        return ticksToRadians(getPositionTicks());
     }
 
     public double getTargetYaw() {
@@ -145,56 +136,6 @@ public class SpindexerSubsystem extends SubsystemBase {
         this.desiredAngle += angle;
     }
 
-    /**
-     * <p>Directly sets the angle of the spindexer.</p>
-     * <p>This is deprecated, and only exists for the tuning files.
-     * See {@link #goToAngle120(double)}, {@link #goToAngle360(double)}, and {@link #rotate(double)} for better alternatives.</p>
-     * @param angle The angle to set the spindexer to.
-     */
-    @Deprecated
-    public void setYaw(double angle) { //angle is in radians cuz i said so oh yeah and also have todo: optimization like the swerve pod thingy where u do the shortest distance
-        this.desiredAngle = angle;
-    }
-
-    public boolean getLimitSwitchState() {
-        return !this.hardware.spindexerLimitSwitch.getState();
-    }
-
-    public void setPidEnabled(boolean enabled) {
-        spindexerPower = 0.0;
-        pidEnabled = enabled;
-    }
-
-    public void updateSpindexer() {
-        this.yawPid.setTargetPosition(radiansToTicks(desiredAngle));
-//        if(hardware.spindexerEncoder.getCurrentPosition())
-        if (pidEnabled) {
-            this.spindexerPower = yawPid.calculatePower(getPositionTicks(), 0);
-        }
-
-        // setting pid power into the spindexer
-    }
-
-    public char[] getBallPositions() {
-        return new char[]{hardware.topSensor.getGreenOrPurple(), hardware.rightSensor.getGreenOrPurple(), hardware.leftSensor.getGreenOrPurple()};
-    }
-
-    public void selectColor(char color) {
-        int nearestIndex = new String(getBallPositions()).indexOf(color);
-        if (nearestIndex == -1) {
-            //TODO: add error handling here some telemetry message abt not having balls or smth
-            return;
-        }
-        goToAngle360(this.yawPid.getTargetPosition() + yawOffsets[nearestIndex]);
-    }
-
-    public double getPositionTicks() {
-        return hardware.spindexerEncoder.getCurrentPosition() - this.homedSpindexerOffset - SPINDEXER_OFFSET_2;
-    }
-
-    public double getPosition() {
-        return ticksToRadians(getPositionTicks());
-    }
 
     public void setWallDown() {
         this.intakeWallPosition1 = INTAKE_WALL_1_DOWN;
@@ -231,25 +172,29 @@ public class SpindexerSubsystem extends SubsystemBase {
         this.homedSpindexerOffset = offset;
     }
 
+    public char[] getBallPositions() {
+        return new char[]{hardware.topSensor.getGreenOrPurple(), hardware.rightSensor.getGreenOrPurple(), hardware.leftSensor.getGreenOrPurple()};
+    }
+
+    public double selectColor(char color) {
+        int nearestIndex = new String(getBallPositions()).indexOf(color);
+        return yawOffsets[nearestIndex];
+    }
+
+
     public void sortBalls() {
+        this.goToAngle120(0);
         double startPos = this.getPositionTicks();
         int fullCount = 0;
         double greenPos = 0.0;
         int greenCount = 0;
         int purpleCount = 0;
-        for (TerrorColorSensor sensor : this.sensors) {
-            if (sensor.getGreenOrPurple() != 'N') {
+        for (char ball : this.getBallPositions()) {
+            if (ball != 'N') {
                 fullCount += 1;
-                if (sensor.getGreenOrPurple() == 'G') {
+                if (ball == 'G') {
                     greenCount += 1;
-                    if (sensor.position.equals(TerrorColorSensor.side.LEFT)) {
-                        greenPos = LEFT_POSITION;
-                    } else if (sensor.position.equals(TerrorColorSensor.side.RIGHT)) {
-                        greenPos = RIGHT_POSITION;
-                    } else {
-                        greenPos = BACK_POSITION;
-                    }
-
+                    greenPos = this.selectColor('G');
                 } else {
                     purpleCount += 1;
                 }
@@ -265,49 +210,62 @@ public class SpindexerSubsystem extends SubsystemBase {
             if (robot.camera.gameGlyph == CameraSubsystem.GLYPH.GPP) {
                 double normalizedError = MathUtils.normalizeRadians((READY_POSITION - greenPos), true);
                 Log.d("spindexer", "glyph gpp normalized error" + normalizedError);
-                if (normalizedError >= 0.1) {
-                    normalizedError = -((2 * Math.PI) - normalizedError);
-                }
-                this.goToAngle360(startPos + normalizedError);
+                this.rotate(startPos + normalizedError);
 
             } else if (robot.camera.gameGlyph == CameraSubsystem.GLYPH.PGP) {
-                double normalizedError = MathUtils.normalizeRadians(((READY_POSITION - ((2D / 3D) * Math.PI)) - greenPos), true);
+                double normalizedError = MathUtils.normalizeRadians(((READY_POSITION + ((2D / 3D) * Math.PI)) - greenPos), true);
                 Log.d("spindexer", "glyph pgp normalized error" + normalizedError);
-                if (normalizedError >= 0.1) {
-                    normalizedError = -((2 * Math.PI) - normalizedError);
-                }
-                this.goToAngle360(startPos + normalizedError);
+                this.rotate(startPos + normalizedError);
 
             } else {
-                double normalizedError = MathUtils.normalizeRadians(((READY_POSITION - ((4D / 3D) * Math.PI)) - greenPos), true);
+                double normalizedError = MathUtils.normalizeRadians(((READY_POSITION + ((4D / 3D) * Math.PI)) - greenPos), true);
                 Log.d("spindexer", "glyph ppg normalized error" + normalizedError);
-                if (normalizedError >= 0.1) {
-                    normalizedError = -((2 * Math.PI) - normalizedError);
-                }
-                this.goToAngle360(startPos + normalizedError);
+                this.rotate(startPos + normalizedError);
             }
         } else {
             Log.d("spindexer", "not enough balls to run logic ready pos:" + READY_POSITION);
-            this.goToAngle360(READY_POSITION);
+            this.rotate(READY_POSITION);
         }
 
+    }
+
+    /**
+     * <p>Directly sets the angle of the spindexer.</p>
+     * <p>This is deprecated, and only exists for the tuning files.
+     * See {@link #goToAngle120(double)}, {@link #goToAngle360(double)}, and {@link #rotate(double)} for better alternatives.</p>
+     * @param angle The angle to set the spindexer to.
+     */
+    @Deprecated
+    public void setYaw(double angle) { //angle is in radians cuz i said so oh yeah and also have todo: optimization like the swerve pod thingy where u do the shortest distance
+        this.desiredAngle = angle;
     }
 
     public void setSpindexerPower(double power) {
         this.spindexerPower = power;
     }
 
+    public boolean getLimitSwitchState() {
+        return !this.hardware.spindexerLimitSwitch.getState();
+    }
+
+    public void setPidEnabled(boolean enabled) {
+        spindexerPower = 0.0;
+        pidEnabled = enabled;
+    }
+
+    public void updateSpindexer() {
+        this.yawPid.setTargetPosition(radiansToTicks(desiredAngle));
+        if (pidEnabled) {
+            this.spindexerPower = yawPid.calculatePower(getPositionTicks(), 0);
+        }
+
+    }
+
+
+
     @Override
     public void periodic() {
-        //to the spindexers of Australia: Robot.camera.getBalls();
-        //G:green P:purple N:none
-        //0:top 1:right 2:left
-        //returns char[]
 
-//        this.hardware.spindexerIntakeRampServo1.setPosition(this.intakeRampPosition1);
-//        this.hardware.spindexerIntakeRampServo2.setPosition(this.intakeRampPosition2);
-//        this.hardware.spindexerTransferRampServo.setPosition(this.shooterRampPosition);
-//        this.hardware.spindexerDiddyServo.setPosition(this.wallPosition);
 
         if (robot.hang.isPtoEngaged()) {
             return;
