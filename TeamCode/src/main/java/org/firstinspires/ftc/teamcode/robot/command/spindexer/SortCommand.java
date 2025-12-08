5ad83cc54d34d0b946ef500b92f190f9aa94f590
aaguy-hue <aaditmathur6@gmail.com>
2025-12-07 22:34:34 -0800

    tele sorting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/spindexer/SortCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/spindexer/SortCommand.java
@@ -5,7 +5,7 @@ import com.seattlesolvers.solverslib.command.InstantCommand;
 import org.firstinspires.ftc.teamcode.robot.subsystems.SpindexerSubsystem;
 
 public class SortCommand extends InstantCommand {
-    public SortCommand (SpindexerSubsystem spindexer){
-        super(() -> spindexer.sortBalls());
+    public SortCommand(SpindexerSubsystem spindexer){
+        super(spindexer::sortBalls);
     }
 }
