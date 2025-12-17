8ca84b6a775df18906ee72947c38452bb5a373f0
Serpintini <136918763+Serpintini@users.noreply.github.com>
2025-12-16 17:25:26 -0800

    selkjr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/BetterShooterAimTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/BetterShooterAimTuner.java
@@ -116,6 +116,7 @@ public class BetterShooterAimTuner extends LinearOpMode {
             robot.shooter.manualAim(velocity, hoodAngle, turretPos);
             robot.spindexer.enableRamp();
             robot.intake.setSpeed(DEFAULT_SPEED);
+            robot.spindexer.setWallDown();
 
             if (robot.robotState == INTAKING){
                 Log.d("data point", "distance: "+ Math.sqrt(Math.pow(robot.follower.getPose().getX()-robot.shooter.goalPosLookupTable.get().x, 2) + Math.pow(robot.follower.getPose().getY()-robot.shooter.goalPosLookupTable.get().y, 2))+ " velocoity: " + robot.shooter.getVelocityRpm()/6.469 + " hood angle: " + hoodAngle);
