8b1dc4dfce13498756fa5e08719b7226330ef447
Serpintini <136918763+Serpintini@users.noreply.github.com>
2025-12-16 17:18:46 -0800

    change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/BetterShooterAimTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/BetterShooterAimTuner.java
@@ -6,6 +6,7 @@ import static org.firstinspires.ftc.teamcode.robot.init.RobotState.INTAKING;
 import static org.firstinspires.ftc.teamcode.robot.init.RobotState.READY_TO_SHOOT;
 import static org.firstinspires.ftc.teamcode.robot.init.RobotState.RESTING;
 import static org.firstinspires.ftc.teamcode.robot.init.RobotState.SHOOTING;
+import static org.firstinspires.ftc.teamcode.robot.subsystems.IntakeSubsystem.DEFAULT_SPEED;
 
 import android.util.Log;
 
@@ -77,100 +78,6 @@ public class BetterShooterAimTuner extends LinearOpMode {
                         () -> (double) gamepad1.right_stick_x, robot)
                 );
 
-        GamepadButton hangButton = new GamepadButton(gamepad1ex, GamepadKeys.Button.Y);
-        Trigger intakeButton = new Trigger(() -> gamepad1ex.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.3);
-        Trigger reverseIntakeButton = new Trigger(() -> gamepad1ex.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.3);
-        GamepadButton rejectButton = new GamepadButton(gamepad1ex, GamepadKeys.Button.A);
-        GamepadButton restingButton = new GamepadButton(gamepad1ex, GamepadKeys.Button.X);
-
-        GamepadButton shoot3button = new GamepadButton(gamepad1ex, GamepadKeys.Button.RIGHT_BUMPER);
-        GamepadButton shoot1button = new GamepadButton(gamepad1ex, GamepadKeys.Button.LEFT_BUMPER);
-
-        GamepadButton resetPinpointButton = new GamepadButton(gamepad1ex, GamepadKeys.Button.BACK);
-
-        GamepadButton adjustTurretLeft = new GamepadButton(gamepad1ex, GamepadKeys.Button.DPAD_LEFT);
-        GamepadButton adjustTurretRight = new GamepadButton(gamepad1ex, GamepadKeys.Button.DPAD_RIGHT);
-
-        GamepadButton sortButton = new GamepadButton(gamepad1ex, GamepadKeys.Button.DPAD_UP);
-
-        Trigger threeBallsAreInside = new Trigger(() -> {
-            final char[] balls = robot.spindexer.getBallPositions();
-            return balls[0] != 'N' && balls[1] != 'N' && balls[2] != 'N';
-        });
-
-        hangButton.whenPressed(new GoToClimbStateCommand(robot));
-        intakeButton.whenActive(new ConditionalCommand(
-                new SequentialCommandGroup(
-                        new GoToIntakeStateCommand(robot)
-//                    new WaitForIntakeCommand(robot),
-//                    new GoToFullStateCommand(robot)
-                ),
-                new InstantCommand(() -> {} ),
-                () ->  robot.robotState != SHOOTING && robot.robotState != READY_TO_SHOOT
-        ));
-        intakeButton.whenInactive(new ConditionalCommand( // if not full state, we will go to resting
-                new GoToRestingStateCommand(robot),
-                new InstantCommand(() -> {} ),
-                () -> robot.robotState != SHOOTING && robot.robotState != READY_TO_SHOOT
-        ));
-
-        reverseIntakeButton.whenActive(
-                new ConditionalCommand(
-                new SetIntakeSpeedCommand(robot.intake, -1.0),
-                new InstantCommand(() -> {} ),
-                () -> robot.robotState != SHOOTING //robot.robotState != FULL
-        ));
-        reverseIntakeButton.whenInactive(new SetIntakeSpeedCommand(robot.intake, 0.0));
-
-        threeBallsAreInside.whenActive(new ConditionalCommand(
-                new PrepareShootCommand(robot),
-                new InstantCommand(() -> {} ),
-                () -> robot.robotState == RESTING || robot.robotState == INTAKING
-        ));
-
-        shoot3button.whenPressed(new ConditionalCommand(
-                new ConditionalCommand( // if we already did the transfer, just shoot immediately
-                        new ShootThreeBallsCommand(robot),
-                        new SequentialCommandGroup(
-                                new PrepareShootCommand(robot),
-                                new ShootThreeBallsCommand(robot)
-                        ),
-                        () -> robot.robotState == READY_TO_SHOOT
-                ),
-                new InstantCommand(() -> {} ),
-                () -> robot.robotState != SHOOTING
-        ));
-
-        shoot1button.whenPressed(new ConditionalCommand(
-                new PrepareShootCommand(robot),
-                new InstantCommand(() -> {} ),
-                () -> robot.robotState != SHOOTING
-        ));
-
-        rejectButton.whenPressed(new ConditionalCommand(
-                new StartShooterRejectCommand(robot.shooter),
-                new InstantCommand(() -> {} ),
-                () -> robot.robotState != SHOOTING //robot.robotState == FULL
-        ));
-
-        restingButton.whenPressed(new GoToRestingStateCommand(robot));
-
-        resetPinpointButton.whenPressed(new InstantCommand(() -> robot.follower.setStartingPose(robot.follower.getPose())));
-
-        adjustTurretLeft.whileHeld(new AdjustTurretOffsetCommand(robot, false));
-        adjustTurretRight.whileHeld(new AdjustTurretOffsetCommand(robot, true));
-
-        sortButton.whenPressed(robot::toggleAutoSort);
-
-        // driver 2
-        GamepadButton motifPGPButton = new GamepadButton(gamepad2ex, GamepadKeys.Button.X);
-        GamepadButton motifGPPButton = new GamepadButton(gamepad2ex, GamepadKeys.Button.Y);
-        GamepadButton motifPPGButton = new GamepadButton(gamepad2ex, GamepadKeys.Button.B);
-
-
-        motifPGPButton.whenPressed(new InstantCommand(() -> robot.camera.gameGlyph= CameraSubsystem.GLYPH.PGP ));
-        motifGPPButton.whenPressed(new InstantCommand(() -> robot.camera.gameGlyph= CameraSubsystem.GLYPH.GPP ));
-        motifPPGButton.whenPressed(new InstantCommand(() -> robot.camera.gameGlyph= CameraSubsystem.GLYPH.PPG ));
 
         //homing command executing here
 
@@ -196,6 +103,7 @@ public class BetterShooterAimTuner extends LinearOpMode {
 
             robot.shooter.manualAim(velocity, hoodAngle, turretPos);
             robot.spindexer.enableRamp();
+            robot.intake.setSpeed(DEFAULT_SPEED);
 
             if (robot.robotState == INTAKING){
                 Log.d("data point", "distance: "+ Math.sqrt(Math.pow(robot.follower.getPose().getX()-robot.shooter.goalPosLookupTable.get().x, 2) + Math.pow(robot.follower.getPose().getY()-robot.shooter.goalPosLookupTable.get().y, 2))+ " velocoity: " + robot.shooter.getVelocityRpm()/6.469 + " hood angle: " + hoodAngle);
