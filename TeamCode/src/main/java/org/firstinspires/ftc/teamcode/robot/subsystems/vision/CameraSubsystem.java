e3d75dfec72fc4783681fa53104f281ce102c04b
aaguy-hue <aaditmathur6@gmail.com>
2026-01-07 18:36:01 -0800

    scan glyphs after dealy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/CameraSubsystem.java
@@ -32,6 +32,8 @@ public class CameraSubsystem extends SubsystemBase {
 
     private AprilTagProcessor aTagProcessor;
 
+    private boolean shouldScanForGlyphs = false;
+
     public enum GLYPH {
         GPP, PGP, PPG
     }
@@ -42,7 +44,7 @@ public class CameraSubsystem extends SubsystemBase {
     public RobotHardware hardware;
 
     public GLYPH gameGlyph;
-    private boolean decodedGlyph = false; //when the movie uses the title of the movie
+    private boolean decodedGlyph = true; //when the movie uses the title of the movie
 
     private final VisionPortal.Builder vPortalBuilder = new VisionPortal.Builder();
     public final VisionPortal vPortalField;
@@ -54,6 +56,7 @@ public class CameraSubsystem extends SubsystemBase {
 
     public CameraSubsystem() {
         this.vPortalField = null;
+        this.shouldScanForGlyphs = true;
     }
 
     public CameraSubsystem(Robot robot, RobotHardware hardware, LiveViewSettings liveViewSettings) {
@@ -81,6 +84,7 @@ public class CameraSubsystem extends SubsystemBase {
 
         vPortalField = vPortalFieldBuilder.build();
 //        vPortalSpindexer = vPortalSpindexerBuilder.build();
+        this.shouldScanForGlyphs = true;
     }
 
     private AprilTagProcessor createAprilTagProcessor() {
@@ -97,6 +101,14 @@ public class CameraSubsystem extends SubsystemBase {
                 .setNumThreads(3) // TODO: the default is 3 but maybe we can change
                 .setLensIntrinsics(910.121, 910.121, 648.374, 394.354)
                 .build();
+    }
+
+    public void stopScanningForGlyphs() {
+        this.shouldScanForGlyphs = false;
+    }
+
+    public void startScanningForGlyphs() {
+        this.shouldScanForGlyphs = true;
     }
 
     public GLYPH getGlyph() {
@@ -138,7 +150,7 @@ public class CameraSubsystem extends SubsystemBase {
 
         for (AprilTagDetection tag : detections) {
             if (tag.id >= 21 && tag.id <= 23) {
-                if (!decodedGlyph)
+                if (!decodedGlyph && shouldScanForGlyphs)
                     setGlyph(GLYPH.valueOf(VisionConstants.APRILTAG.tagMap.get(tag.id)));
             } else {
                 localizationTag = tag;
