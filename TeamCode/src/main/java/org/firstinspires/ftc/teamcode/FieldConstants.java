e9cdfb69c5f2cf23aeda31d6218b01b429df421d
Anonymous Member 1 <anonymous@example.com>
2026-01-11 11:58:12 -0800

    asdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldConstants.java
@@ -7,8 +7,8 @@ import org.firstinspires.ftc.teamcode.math.Pose2d;
 
 @Config
 public class FieldConstants {
-    public static Pose2d BLUE_GOAL_POS = new Pose2d(11, 139, 0.0);
-    public static Pose2d RED_GOAL_POS = new Pose2d(144 - 10, 138, 0.0);
+    public static Pose2d BLUE_GOAL_POS = new Pose2d(0, 154, 0.0);
+    public static Pose2d RED_GOAL_POS = new Pose2d(144, 142, 0.0);
 
     public static Pose2d BLUE_START_POS_NEAR = new Pose2d(20, 123, Math.toRadians(-37.7));
     public static Pose2d RED_START_POS_NEAR = new Pose2d(124, 123, Math.toRadians(217.7));
