5e8ef98cdceaccecf2090cf07b6cc31ff27df3d0
aaguy-hue <aaditmathur6@gmail.com>
2026-01-23 18:52:44 -0800

    remove some unused classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/init/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/init/Robot.java
@@ -17,7 +17,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.robot.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.robot.subsystems.HangSubsystem;
 import org.firstinspires.ftc.teamcode.robot.subsystems.IntakeSubsystem;
-import org.firstinspires.ftc.teamcode.robot.subsystems.LightControl2;
+import org.firstinspires.ftc.teamcode.robot.subsystems.LightControl;
 import org.firstinspires.ftc.teamcode.robot.subsystems.ShooterSubsystem;
 import org.firstinspires.ftc.teamcode.robot.subsystems.SpindexerSubsystem;
 import org.firstinspires.ftc.teamcode.robot.subsystems.vision.CameraSubsystem;
@@ -52,7 +52,7 @@ public class Robot extends com.seattlesolvers.solverslib.command.Robot {
     public CameraSubsystem camera;
     public HangSubsystem hang;
     public IntakeSubsystem intake;
-    public LightControl2 lightControl;
+    public LightControl lightControl;
 
     public static boolean USE_SOTM = false;
     public Pose2d goalPos;
@@ -88,7 +88,7 @@ public class Robot extends com.seattlesolvers.solverslib.command.Robot {
         this.spindexer = new SpindexerSubsystem(hardware, this);
         this.intake = new IntakeSubsystem(this);
         this.hang = new HangSubsystem(hardware);
-        this.lightControl = new LightControl2(hardware,this);
+        this.lightControl = new LightControl(hardware,this);
 
         // Other
         this.setAutoSort(false);
