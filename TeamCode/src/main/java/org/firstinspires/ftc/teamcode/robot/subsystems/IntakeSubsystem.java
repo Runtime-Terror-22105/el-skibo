dee024b6ae6af957ec1e4a2e5a265123bf8f4ff6
Anonymous Member 1 <anonymous@example.com>
2026-01-23 20:06:57 -0800

    half reverse power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/IntakeSubsystem.java
@@ -12,7 +12,7 @@ import org.firstinspires.ftc.teamcode.util.Profiler;
 @Config
 public class IntakeSubsystem extends SubsystemBase {
     public static double DEFAULT_SPEED = 0.9;
-    public static double REVERSE_SPEED = -0.9;
+    public static double REVERSE_SPEED = -0.5;
 
     public static boolean debug = false;
 
