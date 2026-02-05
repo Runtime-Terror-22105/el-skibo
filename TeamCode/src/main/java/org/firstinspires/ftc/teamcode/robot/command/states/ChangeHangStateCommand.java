cdc080e634e6cc46181f19bdfe8a6badae0ab1aa
aaguy-hue <aaditmathur6@gmail.com>
2026-02-04 16:50:42 -0800

    hang bugfix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/states/ChangeHangStateCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/states/ChangeHangStateCommand.java
@@ -1,16 +1,17 @@
 package org.firstinspires.ftc.teamcode.robot.command.states;
 
+import com.seattlesolvers.solverslib.command.ConditionalCommand;
 import com.seattlesolvers.solverslib.command.InstantCommand;
-import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotState;
 
-public class GoToHang90StateCommand extends SequentialCommandGroup {
-    public GoToHang90StateCommand(Robot robot) {
+public class ChangeHangStateCommand extends ConditionalCommand {
+    public ChangeHangStateCommand(Robot robot) {
         super(
+                new InstantCommand(() -> robot.robotState = RobotState.HANGING_FINAL),
                 new InstantCommand(() -> robot.robotState = RobotState.HANGING_90),
-                new InstantCommand(() -> robot.hang.goTo90DegreePosition())
-        )
+                () -> robot.robotState == RobotState.HANGING_90
+        );
     }
 }
