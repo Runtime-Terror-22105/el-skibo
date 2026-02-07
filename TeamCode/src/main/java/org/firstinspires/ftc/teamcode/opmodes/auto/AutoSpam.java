47e7c8d3f5e46f2047c28e4dc7f837eaa38b45b8
aaguy-hue <aaditmathur6@gmail.com>
2026-02-07 09:43:28 -0800

    tweak gate pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoSpam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoSpam.java
@@ -80,9 +80,9 @@ public abstract class AutoSpam extends LinearOpMode {
     public static Pose2d PREPARE_INTAKE_3_POSE = new Pose2d(PREPARE_INTAKE_1_POSE.x, 37, Math.toRadians(180));
     public static Pose2d INTAKE_3_POSE = new Pose2d(20, 39, Math.toRadians(180));
 
-    public static Pose2d GATE_CONTROL_POSE = new Pose2d(55, 61, Math.toRadians(180));
-    public static Pose2d BEFORE_GATE = new Pose2d(22.542, 62.2, Math.toRadians(157));
-    public static Pose2d AFTER_GATE = new Pose2d(10, 62.2, Math.toRadians(162));
+    public static Pose2d GATE_CONTROL_POSE = new Pose2d(55, 59.5, Math.toRadians(180));
+//    public static Pose2d BEFORE_GATE = new Pose2d(22.542, 62.2, Math.toRadians(157));
+    public static Pose2d AFTER_GATE = new Pose2d(10, 61.7, Math.toRadians(162));
 
     public static int PRE_INTAKE_DELAY = 0;
     public static int INTAKE_DELAY = 600;
