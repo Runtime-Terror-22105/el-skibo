103bd63ff519ed393f95355f1b90edd9d6d90f90
Jonathan Liu <jonliu168@gmail.com>
2026-02-06 18:30:00 -0800

    LOCKED CHIN.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/VisionConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/VisionConstants.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems.vision;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class VisionConstants {
@@ -22,16 +25,22 @@ public class VisionConstants {
 
         //if you see two pairs at the start of the game, it's always one of these cases
 
-        public static Map<Integer[], Integer> BlueObeliskPairs = new HashMap<Integer[], Integer>() {{
-            put(new Integer[]{21,22}, 21);
-            put(new Integer[]{22,23}, 22);
-            put(new Integer[]{21,23},23);
+//        public static Map<Integer[], Integer> BlueObeliskPairs = new HashMap<Integer[], Integer>() {{
+//            put(new Integer[]{21,22}, 21);
+//            put(new Integer[]{22,23}, 22);
+//            put(new Integer[]{21,23},23);
+//        }};
+
+        public static HashMap<List<Integer>, Integer> BlueObeliskPairs = new HashMap<List<Integer>, Integer>() {{
+            put(Collections.unmodifiableList(Arrays.asList(21, 22)), 21);
+            put(Collections.unmodifiableList(Arrays.asList(22, 23)), 22);
+            put(Collections.unmodifiableList(Arrays.asList(21, 23)), 23);
         }};
 
-        public static Map<Integer[], Integer> RedObeliskPairs = new HashMap<Integer[], Integer>() {{
-            put(new Integer[]{21,23}, 21);
-            put(new Integer[]{22,23}, 23);
-            put(new Integer[]{21,22},22);
+        public static HashMap<List<Integer>, Integer> RedObeliskPairs = new HashMap<List<Integer>, Integer>() {{
+            put(Collections.unmodifiableList(Arrays.asList(21, 23)), 21);
+            put(Collections.unmodifiableList(Arrays.asList(22, 23)), 23);
+            put(Collections.unmodifiableList(Arrays.asList(21, 22)), 22);
         }};
         // obelisk
         //     22 Δ 23
