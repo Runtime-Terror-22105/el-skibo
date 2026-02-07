4736e51c05c7151e2b9f13924e2a497e9f4fc9bc
Anonymous Member 1 <anonymous@example.com>
2026-02-07 12:20:12 -0800

    add no push gate 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoSpamNoPushGate2.java
@@ -0,0 +1,355 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.FieldConstants.AUTO_ENDING_DATA_KEY;
+import static org.firstinspires.ftc.teamcode.FieldConstants.SPINDEXER_POSITION_KEY;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.CommandScheduler;
+import com.seattlesolvers.solverslib.command.ConditionalCommand;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
+import com.seattlesolvers.solverslib.command.WaitCommand;
+import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
+
+import org.firstinspires.ftc.teamcode.Team;
+import org.firstinspires.ftc.teamcode.math.Pose2d;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.pedroPathing.FtcDashDrawing;
+import org.firstinspires.ftc.teamcode.robot.command.WaitForIntakeCommand;
+import org.firstinspires.ftc.teamcode.robot.command.shooter.ShootThreeBallsCommand;
+import org.firstinspires.ftc.teamcode.robot.command.spindexer.PrepareShootCommand;
+import org.firstinspires.ftc.teamcode.robot.command.spindexer.WaitForSpindexerYawCommand;
+import org.firstinspires.ftc.teamcode.robot.command.states.GoToIntakeStateCommand;
+import org.firstinspires.ftc.teamcode.robot.command.states.GoToRestingStateCommand;
+import org.firstinspires.ftc.teamcode.robot.init.Robot;
+import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
+import org.firstinspires.ftc.teamcode.util.Profiler;
+
+
+// Gate spam auto
+@Config
+public abstract class AutoSpamNoPushGate2 extends LinearOpMode {
+//    public static Map<CameraSubsystem.GLYPH,CameraSubsystem.GLYPH> blueMotifMap = new HashMap<>();
+//    public static Map<CameraSubsystem.GLYPH,CameraSubsystem.GLYPH> redMotifMap = new HashMap<>();
+//    static {
+//        blueMotifMap.put(CameraSubsystem.GLYPH.PPG, CameraSubsystem.GLYPH.PGP);
+//        blueMotifMap.put(CameraSubsystem.GLYPH.PGP, CameraSubsystem.GLYPH.GPP);
+//        blueMotifMap.put(CameraSubsystem.GLYPH.GPP, CameraSubsystem.GLYPH.PPG);
+//
+//        redMotifMap.put(CameraSubsystem.GLYPH.PPG, CameraSubsystem.GLYPH.GPP);
+//        redMotifMap.put(CameraSubsystem.GLYPH.GPP, CameraSubsystem.GLYPH.PGP);
+//        redMotifMap.put(CameraSubsystem.GLYPH.PGP, CameraSubsystem.GLYPH.PPG);
+//    }
+
+    public static boolean stopAfterPreload = false;
+
+    public static double MAX_POWER = 1.0;
+
+    public static PathConstraints RELAXED_CONSTRAINTS;
+    static {
+        RELAXED_CONSTRAINTS = Constants.pathConstraints.copy();
+        RELAXED_CONSTRAINTS.setTValueConstraint(0.93);
+        RELAXED_CONSTRAINTS.setVelocityConstraint(10);
+        RELAXED_CONSTRAINTS.setTranslationalConstraint(5);
+        RELAXED_CONSTRAINTS.setHeadingConstraint(0.07);
+        RELAXED_CONSTRAINTS.setTimeoutConstraint(0);
+    }
+
+    public static Pose2d SHOOT_PRELOAD_POSE = new Pose2d(45, 95, Math.toRadians(225));
+    public static Pose2d SHOOT_EDGE_POSE = new Pose2d(45, 95, Math.toRadians(225));
+    public static Pose2d SHOOT_LAST_POSE = new Pose2d(50, 116, Math.toRadians(315));
+
+    public static Pose2d PREPARE_INTAKE_1_POSE = new Pose2d(52.598, 85.149, Math.toRadians(45));
+    public static Pose2d INTAKE_1_CONTROL = new Pose2d(58, 83, 0);
+    public static Pose2d INTAKE_1_POSE = new Pose2d(25, 85.149, Math.toRadians(180));
+
+    public static Pose2d PREPARE_INTAKE_2_POSE = new Pose2d(PREPARE_INTAKE_1_POSE.x, 60, Math.toRadians(180));
+    public static Pose2d INTAKE_2_CONTROL = new Pose2d(58, 58, 0);
+    public static Pose2d INTAKE_2_POSE = new Pose2d(20, 60, Math.toRadians(180));
+    public static Pose2d BEFORE_PUSH_GATE_POSE = new Pose2d(25, 65, Math.toRadians(180));
+    public static Pose2d PUSH_GATE_POSE = new Pose2d(18.5, 70, Math.toRadians(180));
+
+    public static Pose2d PREPARE_INTAKE_3_POSE = new Pose2d(PREPARE_INTAKE_1_POSE.x, 37, Math.toRadians(180));
+    public static Pose2d INTAKE_3_POSE = new Pose2d(20, 39, Math.toRadians(180));
+
+    public static Pose2d GATE_CONTROL_POSE = new Pose2d(55, 59.5, Math.toRadians(180));
+//    public static Pose2d BEFORE_GATE = new Pose2d(22.542, 62.2, Math.toRadians(157));
+    public static Pose2d AFTER_GATE = new Pose2d(10, 63, Math.toRadians(167));
+
+    public static int PRE_INTAKE_DELAY = 0;
+    public static int INTAKE_DELAY = 600;
+    public static int GATE_INTAKE_DELAY = 1500;
+    public static int PRELOAD_PRE_SHOOT_DELAY = 250;
+    public static int PRE_SHOOT_DELAY = 0;
+    public static int SHOOT_DELAY = 0;
+
+    public static double MAX_DRIVETRAIN_POWER_INTAKING = 0.8;
+
+    private final RobotHardware hardware = new RobotHardware();
+    private final Robot robot = new Robot();
+    private final Team team;
+
+    private PathChain shootPreloadPath;
+    private PathChain intake1Path, shoot1Path;
+    private PathChain intake2Path, shoot2Path;
+    private PathChain beforePushGate2Path, pushGateIntake2Path;
+    private PathChain hitGate1Path, gateToShoot1Path;
+    private PathChain hitGate2Path, gateToShoot2Path;
+
+    private Command shootPreloadCommand;
+    private Command intake1Command, shoot1Command;
+    private Command intake2Command, shoot2Command;
+    private Command intakeGate1Command, shootGate1Command;
+    private Command intakeGate2Command, shootGate2Command;
+
+    double turretAngleForMotif;
+
+    private boolean hasFinished = false;
+    private long duration = 0;
+    private long startTime = 0;
+    private long lastLoop = System.nanoTime();
+
+    protected AutoSpamNoPushGate2(Team team) {
+        this.team = team;
+        robot.goalPos = team.getGoalPos();
+        robot.color = team;
+    }
+
+    private void buildPaths(Pose2d startPose, boolean mirror) {
+        Pose shootPreloadPose = SHOOT_PRELOAD_POSE.toPedro();
+        Pose shootEdgePose = SHOOT_EDGE_POSE.toPedro();
+        Pose shootLastPose = SHOOT_LAST_POSE.toPedro();
+        Pose prepareIntake3Pose = PREPARE_INTAKE_3_POSE.toPedro();
+        Pose intake3Control = new Pose(56.751, 45.668);
+        Pose intake3Pose = INTAKE_3_POSE.toPedro();
+
+        if (mirror) {
+            shootPreloadPose = shootPreloadPose.mirror();
+            shootEdgePose = shootEdgePose.mirror();
+            shootLastPose = shootLastPose.mirror();
+            prepareIntake3Pose = prepareIntake3Pose.mirror();
+            intake3Control = intake3Control.mirror();
+            intake3Pose = intake3Pose.mirror();
+        }
+
+        shootPreloadPath = PathUtil.addPathBuilderLine(robot, startPose, new Pose2d(shootPreloadPose), false, false, false)
+                .setConstraintsForLast(RELAXED_CONSTRAINTS)
+                .build();
+
+        intake1Path = PathUtil.addPathBuilderCurve(robot, shootPreloadPath, INTAKE_1_CONTROL, INTAKE_1_POSE, mirror, false, false)
+                .setConstraintsForLast(RELAXED_CONSTRAINTS)
+                .build();
+        shoot1Path = PathUtil.addPathBuilderLine(robot, intake1Path, SHOOT_EDGE_POSE, mirror, true, true)
+                .setConstraintsForLast(RELAXED_CONSTRAINTS)
+                .build();
+
+        intake2Path = PathUtil.addPathBuilderCurve(robot, shoot1Path, INTAKE_2_CONTROL, INTAKE_2_POSE, mirror, false, false)
+//                .setConstraintsForLast(RELAXED_CONSTRAINTS)
+                .build();
+        shoot2Path = PathUtil.addPathBuilderLine(robot, intake2Path, SHOOT_EDGE_POSE, mirror, true, true)
+                .setConstraintsForLast(RELAXED_CONSTRAINTS)
+                .build();
+
+        hitGate1Path = PathUtil.createCurvePath(robot, shoot2Path, GATE_CONTROL_POSE, AFTER_GATE, mirror, false, false);
+        gateToShoot1Path = PathUtil.addPathBuilderLine(robot, hitGate1Path, SHOOT_EDGE_POSE, mirror, true, true)
+                .setConstraintsForLast(RELAXED_CONSTRAINTS)
+                .build();
+        hitGate2Path = PathUtil.createCurvePath(robot, gateToShoot1Path, GATE_CONTROL_POSE, AFTER_GATE, mirror, false, false);
+        gateToShoot2Path = PathUtil.addPathBuilderLine(robot, hitGate2Path, SHOOT_LAST_POSE, mirror, true, true)
+                .setConstraintsForLast(RELAXED_CONSTRAINTS)
+                .build();
+    }
+
+    private void buildCommands() {
+        shootPreloadCommand = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new PrepareShootCommand(robot),
+                        new FollowPathCommand(robot.follower, shootPreloadPath, false)
+                ),
+                new WaitCommand(PRELOAD_PRE_SHOOT_DELAY),
+                new ShootThreeBallsCommand(robot),
+                new WaitForSpindexerYawCommand(robot.spindexer).withTimeout(500),
+//                new InstantCommand(() -> robot.camera.stopScanningForGlyphs()),
+                new WaitCommand(SHOOT_DELAY)
+        );
+
+        intake1Command = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new FollowPathCommand(robot.follower, intake1Path, true, MAX_DRIVETRAIN_POWER_INTAKING),
+                        new GoToIntakeStateCommand(robot)
+                ),
+                new WaitForIntakeCommand(robot).withTimeout(INTAKE_DELAY)
+        );
+        shoot1Command = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new FollowPathCommand(robot.follower, shoot1Path, false),
+                        new WaitCommand(250).andThen(new PrepareShootCommand(robot))
+                ),
+                new WaitCommand(PRE_SHOOT_DELAY),
+                new ShootThreeBallsCommand(robot),
+                new WaitForSpindexerYawCommand(robot.spindexer).withTimeout(2000),
+                new WaitCommand(SHOOT_DELAY)
+        );
+
+        intake2Command = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new FollowPathCommand(robot.follower, intake2Path, true, MAX_DRIVETRAIN_POWER_INTAKING),
+                        new GoToIntakeStateCommand(robot)
+                ),
+                new WaitCommand(INTAKE_DELAY)
+        );
+        shoot2Command = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new FollowPathCommand(robot.follower, shoot2Path, false),
+                        new WaitCommand(250).andThen(new PrepareShootCommand(robot))
+                ),
+                new WaitCommand(PRE_SHOOT_DELAY),
+                new ShootThreeBallsCommand(robot),
+                new WaitForSpindexerYawCommand(robot.spindexer).withTimeout(2000),
+                new WaitCommand(SHOOT_DELAY)
+        );
+
+        intakeGate1Command = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new FollowPathCommand(robot.follower, hitGate1Path, true, MAX_DRIVETRAIN_POWER_INTAKING),
+                        new GoToIntakeStateCommand(robot)
+                ),
+                new WaitForIntakeCommand(robot).withTimeout(GATE_INTAKE_DELAY)
+        );
+        shootGate1Command = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new FollowPathCommand(robot.follower, gateToShoot1Path, false),
+                        new WaitCommand(250).andThen(new PrepareShootCommand(robot, true))
+                ),
+                new WaitCommand(PRE_SHOOT_DELAY),
+                new ShootThreeBallsCommand(robot),
+                new WaitForSpindexerYawCommand(robot.spindexer).withTimeout(2000),
+                new WaitCommand(SHOOT_DELAY)
+        );
+
+        intakeGate2Command = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new FollowPathCommand(robot.follower, hitGate2Path, true, MAX_DRIVETRAIN_POWER_INTAKING),
+                        new GoToIntakeStateCommand(robot)
+                ),
+                new WaitForIntakeCommand(robot).withTimeout(GATE_INTAKE_DELAY)
+        );
+        shootGate2Command = new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                        new FollowPathCommand(robot.follower, gateToShoot2Path, false),
+                        new WaitCommand(250).andThen(new PrepareShootCommand(robot, true))
+                ),
+                new WaitCommand(PRE_SHOOT_DELAY),
+                new ShootThreeBallsCommand(robot),
+                new WaitForSpindexerYawCommand(robot.spindexer).withTimeout(2000),
+                new WaitCommand(SHOOT_DELAY)
+        );
+    }
+
+    public void runOpMode() {
+        Profiler.init();
+
+        hardwareMap.dcMotor.get("motorFrontLeft").setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hardwareMap.dcMotor.get("motorFrontLeft").setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+//        hardware.init(hardwareMap, LynxModule.BulkCachingMode.MANUAL, RobotHardware.HardwareOptions.CAMERA);
+        hardware.init(hardwareMap, LynxModule.BulkCachingMode.MANUAL);
+
+        robot.init(hardware, this);
+//        robot.camera.stopScanningForGlyphs();
+
+        this.turretAngleForMotif = Math.PI + (Team.BLUE.equals(team) ? -1 : 1) * Math.toRadians(30);
+        robot.goalPos = team.getGoalPos();
+        robot.follower.setStartingPose(team.getStartPosNear().toPedro());
+
+        buildPaths(team.getStartPosNear(), Team.RED.equals(team));
+        buildCommands();
+        robot.follower.setMaxPower(MAX_POWER);
+
+        // todo note that this will mean we always sort, for 9 balls this is ok but for 12+ we want this to be only in certain cases
+        // todo do the rules require that we do ths after init?
+        robot.setAutoSort(false);
+
+        // we can't spin shooter in init bc it's illegal
+        robot.shooter.isAutoVelOn = false;
+        robot.shooter.setSpeed(0D);
+
+        waitForStart();
+        robot.shooter.isAutoVelOn = true;
+        robot.shooter.isAutoAimOn = true;
+        robot.shooter.isAutoTurretOn = true;
+        robot.shooter.alwaysUpdateTurret = true;
+        startTime = System.currentTimeMillis();
+
+//        robot.camera.stopCamera();
+        CommandScheduler.getInstance().schedule(new SequentialCommandGroup(
+                shootPreloadCommand,
+                new ConditionalCommand(
+                        new SequentialCommandGroup(
+                                intake1Command, shoot1Command,
+                                intake2Command, shoot2Command,
+                                intakeGate1Command, shootGate1Command,
+                                intakeGate2Command, shootGate2Command
+                        ),
+                        new SequentialCommandGroup(
+                                new GoToRestingStateCommand(robot)
+                        ),
+                        () -> !stopAfterPreload
+                ),
+                new InstantCommand(() -> hasFinished = true)
+        ));
+
+        lastLoop = System.nanoTime();
+
+        while (opModeIsActive()) {
+            Profiler.start();
+
+            // Manually clear the bulk read cache. Deleting this would be catastrophic b/c stale
+            // vals would be used.
+            Profiler.push("clear_cache");
+            for (LynxModule hub : hardware.allHubs) {
+                hub.clearBulkCache();
+            }
+            Profiler.pop();
+
+            Profiler.push("commands");
+            CommandScheduler.getInstance().run();
+            Profiler.pop();
+
+//            blackboard.put(MOTIF_DATA_KEY, robot.camera.getGlyph());
+            blackboard.put(AUTO_ENDING_DATA_KEY, robot.follower.getPose());
+            blackboard.put(SPINDEXER_POSITION_KEY, robot.spindexer.getPosition());
+
+            Profiler.push("hwWrite");
+            hardware.write();
+            Profiler.pop();
+
+            Profiler.push("debug");
+            Profiler.push("draw");
+            FtcDashDrawing.drawDebug(robot.follower);
+            Profiler.pop();
+            long time = System.nanoTime();
+            long dt = time - lastLoop;
+            lastLoop = time;
+            robot.telemetry.addData("Loop Time (ms)", String.format("%.2f", dt / 1e6));
+            if (!hasFinished) {
+                duration = System.currentTimeMillis() - startTime;
+            }
+            robot.telemetry.addData("Auto Time (s)", String.format("%.2f", (System.currentTimeMillis() - startTime) / 1000.0));
+            robot.telemetry.addData("Auto Duration (s)", String.format("%.2f", duration / 1000.0));
+            robot.telemetry.update();
+            Profiler.pop();
+
+            Profiler.end();
+            Profiler.sendFlamegraph(robot.telemetry);
+        }
+    }
+}
