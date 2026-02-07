0d242847fdc46736e81fa61c9aacd1e473b4e8cc
Jonathan Liu <jonliu168@gmail.com>
2026-02-06 17:51:25 -0800

    bad job

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/CameraTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes.testing;
 
+import android.util.Log;
+
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,6 +9,7 @@ import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Team;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
 
@@ -19,12 +22,14 @@ public class CameraTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         hardware.init(hardwareMap, LynxModule.BulkCachingMode.AUTO, RobotHardware.HardwareOptions.CAMERA);
         robot.init(hardware, telemetry);
-
+        robot.camera.setTeam(Team.BLUE);
+        robot.camera.setIsInAuto(true);
         waitForStart();
 
         while (opModeIsActive())
         {
             CommandScheduler.getInstance().run();
+//            Log.d("gooning",String.valueOf(robot.camera.getGlyph()));
 
             robot.telemetry.addData("seenglyph",robot.camera.getGlyph());
             //telemetry.addData("position",robot.camera.getPositionCamera());
