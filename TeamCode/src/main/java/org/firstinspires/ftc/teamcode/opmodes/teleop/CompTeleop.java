878ad2e352f0bcae234f236dfa63eb6516aeae2b
Serpintini <136918763+Serpintini@users.noreply.github.com>
2026-02-09 17:15:10 -0800

    created a competition teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CompTeleop.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "*Comp TeleOp*", group = "ATeleOp")
+@Config
+public class CompTeleop extends TerrorTeleOp{
+
+    public CompTeleop(){
+        super();
+    }
+}
