b5ca1e07666452e53292c50ff5269612e5624546
Landon <53448214+lunbun@users.noreply.github.com>
2026-02-11 19:05:20 -0800

    feat: update `AutoConstants` to latest from lm4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/AutoConstants.java
@@ -11,32 +11,28 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
  */
 @Config
 public class AutoConstants {
-    public static Pose2d SHOOT_PRELOAD_POSE = new Pose2d(50.0, 93, Math.toRadians(225));
-    public static Pose2d SHOOT_EDGE_POSE = new Pose2d(60, 83, Math.toRadians(45));
+    public static Pose2d SHOOT_PRELOAD_POSE = new Pose2d(45, 95, Math.toRadians(225));
+    public static Pose2d SHOOT_EDGE_POSE = new Pose2d(45, 95, Math.toRadians(225));
     public static Pose2d SHOOT_LAST_POSE = new Pose2d(50, 116, Math.toRadians(315));
 
-    public static Pose2d PREPARE_INTAKE_1_POSE = new Pose2d(52.598, 85.149, Math.toRadians(180));
     public static Pose2d INTAKE_1_CONTROL = new Pose2d(58, 83, 0);
-    public static Pose2d INTAKE_1_POSE = new Pose2d(25, 85.149, Math.toRadians(180));
+    public static Pose2d INTAKE_1_POSE = new Pose2d(22, 85.149, Math.toRadians(180));
 
-    public static Pose2d PREPARE_INTAKE_2_POSE = new Pose2d(PREPARE_INTAKE_1_POSE.x, 60, Math.toRadians(180));
     public static Pose2d INTAKE_2_CONTROL = new Pose2d(58, 58, 0);
-    public static Pose2d INTAKE_2_POSE = new Pose2d(20, 60, Math.toRadians(180));
+    public static Pose2d INTAKE_2_POSE = new Pose2d(17, 60, Math.toRadians(180));
 
-    public static Pose2d PREPARE_INTAKE_3_POSE = new Pose2d(PREPARE_INTAKE_1_POSE.x, 37, Math.toRadians(180));
+    public static Pose2d PREPARE_INTAKE_3_POSE = new Pose2d(52.598, 37, Math.toRadians(180));
     public static Pose2d INTAKE_3_CONTROL = new Pose2d(56.751, 45.668);
-    public static Pose2d INTAKE_3_POSE = new Pose2d(20, 39, Math.toRadians(180));
+    public static Pose2d INTAKE_3_POSE = new Pose2d(17, 39, Math.toRadians(180));
 
     // For pushing gate after a SPIKE STRIP.
     public static Pose2d BEFORE_PUSH_GATE_POSE = new Pose2d(25, 65, Math.toRadians(180));
     public static Pose2d PUSH_GATE_POSE = new Pose2d(18.5, 70, Math.toRadians(180));
 
     // For CYCLING gate.
-    public static Pose2d GATE_CONTROL_POSE = new Pose2d(55, 61, Math.toRadians(180));
-    public static Pose2d BEFORE_GATE = new Pose2d(22.542, 62.2, Math.toRadians(157));
-    public static Pose2d AFTER_GATE = new Pose2d(11, 62.2, Math.toRadians(157));
+    public static Pose2d GATE_CONTROL_POSE = new Pose2d(55, 59.5, Math.toRadians(180));
+    public static Pose2d AFTER_GATE = new Pose2d(12, 62, Math.toRadians(163));
 
-    public static int PRE_INTAKE_DELAY = 0;
     public static int INTAKE_DELAY = 600;
     public static int GATE_INTAKE_DELAY = 1500;
     public static int PRELOAD_PRE_SHOOT_DELAY = 250;
