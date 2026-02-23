04835fbf56583fda632e883e88910c8884813f3c
aaguy-hue <aaditmathur6@gmail.com>
2026-02-22 18:26:30 -0800

    set last motif to ppg, reduce shooting delay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoSorted12.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoSorted12.java
@@ -2,16 +2,18 @@ package org.firstinspires.ftc.teamcode.opmodes.auto;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.Team;
 import org.firstinspires.ftc.teamcode.robot.auto.AutoBuilder;
 import org.firstinspires.ftc.teamcode.robot.auto.ShootPathFlag;
+import org.firstinspires.ftc.teamcode.robot.subsystems.vision.CameraSubsystem;
 import org.firstinspires.ftc.teamcode.util.StartConfig;
 
 @Config
 public abstract class AutoSorted12 extends OneAutoToRuleThemAll {
-    public static long SHOOTING_DELAY = 2000;
+    public static long SHOOTING_DELAY = 1000;
 
     protected AutoSorted12(Team team) {
         super(team);
@@ -34,6 +36,8 @@ public abstract class AutoSorted12 extends OneAutoToRuleThemAll {
                 builder.shootPreload(),
                 builder.cycleSpike(1),
                 builder.cycleSpike(2),
+                // ppg is more optimal in case we miss in earlier rounds by maximizing purples
+                new InstantCommand(() -> robot.camera.setGlyph(CameraSubsystem.GLYPH.PPG)),
                 builder.cycleSpike(3, ShootPathFlag.LAST)
         );
     }
