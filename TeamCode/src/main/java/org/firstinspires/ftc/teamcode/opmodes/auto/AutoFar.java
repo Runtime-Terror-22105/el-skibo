66723c65e33761f72360c37546c2e71a4c22dfdf
Anonymous Member 1 <anonymous@example.com>
2026-02-28 13:42:56 -0800

    remove tolerance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoFar.java
@@ -27,7 +27,7 @@ public abstract class AutoFar extends OneAutoToRuleThemAll {
     @Override
     protected Command createAutoCommand(AutoBuilder builder) {
         return new SequentialCommandGroup(
-                new InstantCommand(() -> robot.spindexer.setTolerance(10)),
+//                new InstantCommand(() -> robot.spindexer.setTolerance(10)),
                 builder.shootPreloadFar(ShootPathFlag.EARLY_LEAVE),
                 // Do not reverse intake on first since they're guaranteed
                 builder.cycleWall(false, ShootPathFlag.EARLY_SHOOT, ShootPathFlag.EARLY_LEAVE),
