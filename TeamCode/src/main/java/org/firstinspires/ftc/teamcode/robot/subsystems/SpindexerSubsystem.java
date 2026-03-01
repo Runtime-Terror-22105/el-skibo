813c48055d33fc15334df9b5f76be434f5ecebd2
aaguy-hue <aaditmathur6@gmail.com>
2026-03-01 14:51:13 -0800

    remove kstatic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
@@ -54,7 +54,7 @@ public class SpindexerSubsystem extends SubsystemBase {
     double[] yawOffsets = {0, (2.0 / 3) * Math.PI, -((2.0 / 3) * Math.PI)};
 
     public static PidfController.PidfCoefficients turningPidCoefficients =
-            new PidfController.PidfCoefficients(0.279, 0, 0.005, 0, 0.01);
+            new PidfController.PidfCoefficients(0.279, 0, 0.005, 0, 0.0);
     public static double yawPidTolerance = Math.toRadians(4); // radians
     private boolean pidEnabled = true;
     public final PidfController yawPid = new PidfController(turningPidCoefficients);
