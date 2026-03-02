135019ae30ad9917500fb631f556ff2d92a1e238
aaguy-hue <aaditmathur6@gmail.com>
2026-03-01 18:35:13 -0800

    fix err

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoSpamNoPushGate2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoSpamNoPushGate2.java
@@ -30,7 +30,7 @@ public abstract class AutoSpamNoPushGate2 extends OneAutoToRuleThemAll {
                 builder.cycleSpike(1, ShootPathFlag.EARLY_SHOOT, ShootPathFlag.EARLY_LEAVE),
                 builder.cycleSpike(2, ShootPathFlag.EARLY_SHOOT, ShootPathFlag.EARLY_LEAVE),
                 builder.cycleGate(true, ShootPathFlag.EARLY_SHOOT, ShootPathFlag.EARLY_LEAVE),
-                builder.cycleGate(true, ShootPathFlag.EARLY_SHOOT, ShootPathFlag.EARLY_LEAVE)
+                builder.cycleGate(true, ShootPathFlag.EARLY_SHOOT, ShootPathFlag.EARLY_LEAVE),
                 builder.cycleGate(true, ShootPathFlag.LAST, ShootPathFlag.EARLY_SHOOT, ShootPathFlag.EARLY_LEAVE)
         );
     }
