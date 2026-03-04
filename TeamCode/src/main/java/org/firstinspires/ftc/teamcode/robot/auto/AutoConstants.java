67ea55867925240a877c3cd04e36072748a97bf8
Anonymous Member 1 <anonymous@example.com>
2026-03-03 18:30:27 -0800

    improve sorted shoot pose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/AutoConstants.java
@@ -12,9 +12,9 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 @Config
 public class AutoConstants {
     // Unless otherwise specified, all poses are for NEAR ZONE.
-    public static Pose2d SHOOT_PRELOAD_POSE = new Pose2d(48, 103, 4.2);
+    public static Pose2d SHOOT_PRELOAD_POSE = new Pose2d(53, 98, 4.2);
     public static Pose2d SHOOT_PRELOAD_HORIZ_POSE = new Pose2d(24, 115, Math.toRadians(225));
-    public static Pose2d SHOOT_EDGE_POSE = new Pose2d(48, 103, Math.toRadians(-124.7));
+    public static Pose2d SHOOT_EDGE_POSE = new Pose2d(53, 98, Math.toRadians(-124.7));
     public static Pose2d SHOOT_EDGE_HORIZ_POSE = new Pose2d(36, 104, Math.toRadians(225));
     public static Pose2d SHOOT_LAST_POSE = new Pose2d(50, 116, Math.toRadians(315));
     public static Pose2d SHOOT_FAR_POSE = new Pose2d(51, 16, Math.toRadians(195));
