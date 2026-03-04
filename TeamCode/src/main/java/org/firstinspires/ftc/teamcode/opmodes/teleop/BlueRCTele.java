590ab52a6ad101be211c8e3755b73287378391ce
Anonymous Member 1 <anonymous@example.com>
2026-03-03 20:06:40 -0800

    color the teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BlueRCTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BlueRCTele.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Team;
 
-@TeleOp(name = "Blue RC TeleOp", group = "ATeleOp")
+@TeleOp(name = "🟦 Blue RC TeleOp", group = "ATeleOp")
 @Config
 public class BlueRCTele extends TerrorTeleOp {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/RedRCTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/RedRCTele.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Team;
 
-@TeleOp(name = "Red RC TeleOp", group = "ATeleOp")
+@TeleOp(name = "🟥 Red RC TeleOp", group = "ATeleOp")
 @Config
 public class RedRCTele extends TerrorTeleOp {
 
