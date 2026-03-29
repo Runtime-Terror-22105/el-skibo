8b9225f4a361d5fcc66490ad445163335473c79a
aaguy-hue <aaditmathur6@gmail.com>
2026-03-28 17:19:51 -0700

    disable sotm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/ShooterSubsystem.java
@@ -29,8 +29,8 @@ import org.firstinspires.ftc.teamcode.util.Profiler;
 public class ShooterSubsystem extends SubsystemBase {
     public static int ACCEL_BUFFER_SZE = 3;
     public static double ACCELERATION_COEFFICIENT = 0.12;
-    public static boolean USE_SOTM = true;
-    public static boolean USE_SOTM_ACCEL = true;
+    public static boolean USE_SOTM = false;
+    public static boolean USE_SOTM_ACCEL = false;
 
     public static boolean debug = false;
     public static boolean telemetry = true;
