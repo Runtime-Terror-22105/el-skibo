a01a32c4a88cdb630f6effdbded73a307fbeff0b
aaguy-hue <aaditmathur6@gmail.com>
2026-04-08 13:58:37 -0700

    Tune spindex pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
@@ -62,7 +62,7 @@ public class SpindexerSubsystem extends SubsystemBase {
     double[] yawOffsets = {0, (2.0 / 3) * Math.PI, -((2.0 / 3) * Math.PI)};
 
     public static PidfController.PidfCoefficients turningPidCoefficients =
-            new PidfController.PidfCoefficients(0.4, 0, 0.025, 0, 0.12);
+            new PidfController.PidfCoefficients(0.25, 0, 0.0065, 0, 0.09);
     public static double yawPidTolerance = Math.toRadians(4); // radians
     private boolean pidEnabled = true;
     public final PidfController yawPid = new PidfController(turningPidCoefficients);
