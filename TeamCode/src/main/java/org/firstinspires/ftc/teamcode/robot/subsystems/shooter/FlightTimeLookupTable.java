69cad63abf897b39387af29988945ab8141536f8
Serpintini <136918763+Serpintini@users.noreply.github.com>
2026-04-12 16:23:18 -0700

    a mediorce attemt at shooter tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/shooter/FlightTimeLookupTable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/shooter/FlightTimeLookupTable.java
@@ -34,13 +34,13 @@ public class FlightTimeLookupTable {
 
     public static FlightTimeLookupValue[] DATA_POINTS = new FlightTimeLookupValue[]{
 //            new FlightTimeLookupValue(83.5,1.530-.690)
-            new FlightTimeLookupValue(27.7,0.62),
-            new FlightTimeLookupValue(45.5,0.66), //im sorry aadit
-            new FlightTimeLookupValue(68.7,0.68), //im sorry aadit
-            new FlightTimeLookupValue(90.7,0.68),
-            new FlightTimeLookupValue(104.4,0.68),
-            new FlightTimeLookupValue(122.7,0.72),
-            new FlightTimeLookupValue(140.2,0.76),
+            new FlightTimeLookupValue(27.7,0.71),
+            //new FlightTimeLookupValue(45.5,0.66), //im sorry aadit
+            new FlightTimeLookupValue(68.7,0.62), //im sorry aadit
+            new FlightTimeLookupValue(90.7,0.77),
+            //new FlightTimeLookupValue(104.4,0.68),
+            new FlightTimeLookupValue(122.7,0.8),
+            new FlightTimeLookupValue(140.2,0.9),
 
     };
 
