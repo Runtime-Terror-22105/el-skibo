90c75ccbdfeb2dd474a2e79992fb88e76d2e6346
Jonathan Liu <jonliu168@gmail.com>
2026-04-12 16:59:03 -0700

    cv test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/CVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing/CVTest.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
 import org.firstinspires.ftc.teamcode.robot.subsystems.vision.CameraSubsystem;
 
-@TeleOp(name=" V Test", group="Testing")
+@TeleOp(name="CV Test", group="Testing")
 @Config
 public class CVTest extends LinearOpMode {
     private final RobotHardware hardware = new RobotHardware();
