8f3e03999a7cc603f10971ec5cf49185885224bf
aaguy-hue <aaditmathur6@gmail.com>
2026-04-16 18:05:34 -0700

    hood pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/ShooterSubsystem.java
@@ -80,8 +80,8 @@ public class ShooterSubsystem extends SubsystemBase {
     public static double turretServoUpperBound = 1;
 
     // hood limits
-    public static double hoodPosMax = 1.0; //maximum position the servo can go to
-    public static double hoodPosMin = 0.38; //min position the servo can go to
+    public static double hoodPosMax = 0.98; //maximum position the servo can go to
+    public static double hoodPosMin = 0.3; //min position the servo can go to
     public static double hoodAngleMax = 1.0; //radian measure of hood at max pos
     public static double hoodAngleMin = 0.0; //radian measure of hood at min pos
 
