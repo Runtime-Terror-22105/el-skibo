23287328748b31978d6482efe8b152fd1f9c5e2b
Jonathan Liu <jonliu168@gmail.com>
2026-04-17 17:55:17 -0700

    ramppipeline retune

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/RampPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/RampPipeline.java
@@ -23,21 +23,20 @@ public class RampPipeline implements VisionProcessor
 
     public static double acceptPixelsAbove = 820;
 
-    public static Scalar purpleLow1  = new Scalar(45.3, 77.9, 155.8);
-    public static Scalar purpleHigh1 = new Scalar(121.8, 68, 255);
-    public static Scalar purpleLow2  = new Scalar(120.4, 76.5, 48.2);
-    public static Scalar purpleHigh2 = new Scalar(165.8, 255, 255);
-    public static Scalar greenLow  = new Scalar(46, 0, 0);
-    public static Scalar greenHigh = new Scalar(102, 255, 250);
-    Mat purpleMask1 = new Mat();
-    Mat purpleMask2 = new Mat();
+//    public static Scalar purpleLow1  = new Scalar(45.3, 77.9, 155.8);
+//    public static Scalar purpleHigh1 = new Scalar(121.8, 68, 255);
+    public static Scalar purpleLow  = new Scalar(147.3,83.6,45.3);
+    public static Scalar purpleHigh = new Scalar(255, 255, 255);
+    public static Scalar greenLow  = new Scalar(46.8, 109.1, 39.7);
+    public static Scalar greenHigh = new Scalar(87.8,255,184.2);
+//    Mat purpleMask1 = new Mat();
+//    Mat purpleMask2 = new Mat();
     Mat purpleMask = new Mat();
 
     Mat greenMask = new Mat();
 
     private Mat roiMask = new Mat();
 
-    private MatOfPoint maskShape;
     public static Point[] ROI_POINTS = {
             new Point(40,100),
             new Point(40,140),
@@ -64,7 +63,7 @@ public class RampPipeline implements VisionProcessor
     public void init(int width, int height, CameraCalibration calibration) {
         roiMask = new Mat(height, width, 0);
 
-        maskShape = new MatOfPoint();
+        MatOfPoint maskShape = new MatOfPoint();
         maskShape.fromArray(ROI_POINTS);
 
         List<MatOfPoint> polygons = new ArrayList<>();
@@ -77,9 +76,9 @@ public class RampPipeline implements VisionProcessor
 
 
         Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_RGB2HSV);
-        Core.inRange(hsv, purpleLow1, purpleHigh1, purpleMask1);
-        Core.inRange(hsv, purpleLow2, purpleHigh2, purpleMask2);
-        Core.bitwise_or(purpleMask1, purpleMask2, purpleMask);
+//        Core.inRange(hsv, purpleLow1, purpleHigh1, purpleMask1);
+        Core.inRange(hsv, purpleLow, purpleHigh, purpleMask);
+//        Core.bitwise_or(purpleMask1, purpleMask2, purpleMask);
         Core.inRange(hsv, greenLow, greenHigh, greenMask);
 
         Mat combinedMask = new Mat();
