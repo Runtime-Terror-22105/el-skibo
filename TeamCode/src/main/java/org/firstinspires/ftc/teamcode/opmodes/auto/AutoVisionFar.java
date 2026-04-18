52b916ec50929cf8fbdb38d3892f113193d93d43
aaguy-hue <aaditmathur6@gmail.com>
2026-04-17 20:19:57 -0700

    full vision far auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoVisionFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoVisionFar.java
@@ -6,6 +6,7 @@ import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import org.firstinspires.ftc.teamcode.Team;
 import org.firstinspires.ftc.teamcode.robot.auto.AutoBuildState;
 import org.firstinspires.ftc.teamcode.robot.auto.FarAutoBuilder;
+import org.firstinspires.ftc.teamcode.robot.auto.KillTimerCommand;
 import org.firstinspires.ftc.teamcode.robot.auto.ShootPathFlag;
 import org.firstinspires.ftc.teamcode.util.StartConfig;
 
@@ -25,25 +26,27 @@ public abstract class AutoVisionFar extends OneAutoToRuleThemAll {
     }
 
     @Override
+    public boolean wantsBallCv() {
+        return true;
+    }
+
+    @Override
     protected Command createAutoCommand(AutoBuildState state) {
+        state.prepareShootTimeBeforeReverseIntake = 0;
+        state.shootBrakingStrength = 0.75;
         return new SequentialCommandGroup(
-                FarAutoBuilder.shootPreload(state, ShootPathFlag.EARLY_LEAVE),
+                FarAutoBuilder.shootPreload(state),
+
                 // Do not reverse intake on first since they're guaranteed
-                FarAutoBuilder.cycleWall(state, false, ShootPathFlag.EARLY_LEAVE),
                 FarAutoBuilder.intakeSpike3(state),
-                FarAutoBuilder.shootSpike3(state, ShootPathFlag.EARLY_LEAVE),
+                FarAutoBuilder.shootSpike3(state, ShootPathFlag.EARLY_SHOOT, ShootPathFlag.EARLY_LEAVE),
 
                 FarAutoBuilder.prepareVision(state),
                 FarAutoBuilder.cycleVision(state, true, ShootPathFlag.EARLY_LEAVE),
                 FarAutoBuilder.cycleVision(state, true, ShootPathFlag.EARLY_LEAVE),
                 FarAutoBuilder.cycleVision(state, true, ShootPathFlag.EARLY_LEAVE),
                 FarAutoBuilder.cycleVision(state, true, ShootPathFlag.EARLY_LEAVE),
-                FarAutoBuilder.cycleVision(state, true, ShootPathFlag.EARLY_LEAVE),
                 FarAutoBuilder.cycleVision(state, true, ShootPathFlag.EARLY_LEAVE)
-
-
-        );
-
-
+        ).alongWith(new KillTimerCommand(robot));
     }
 }
