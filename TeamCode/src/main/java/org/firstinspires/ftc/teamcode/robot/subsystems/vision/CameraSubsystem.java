c17d5f88170df1d99c3fc6395c96a08a3b4d27c4
Jonathan Liu <jonliu168@gmail.com>
2026-04-17 17:59:02 -0700

    pipeline goodies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/vision/CameraSubsystem.java
@@ -416,6 +416,7 @@ public class CameraSubsystem extends SubsystemBase {
 
     public void setBallsSeen(int ballsOnRamp){
         this.ballsSeen = ballsOnRamp;
+        robot.telemetry.addData("Balls Seen", ballsOnRamp);
     }
 
     public void setGlyphScanningEnabled(boolean state)
