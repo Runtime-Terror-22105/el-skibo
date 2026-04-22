7c290079b2bd45f5b1de186a1feeda0fbc5e6835
aaguy-hue <aaditmathur6@gmail.com>
2026-04-21 18:24:09 -0700

    new hood pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/shooter/ToggleAutoTurretCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/command/shooter/ToggleAutoTurretCommand.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.robot.command.shooter;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
-import org.firstinspires.ftc.teamcode.robot.subsystems.ShooterSubsystem;
 
 public class ToggleAutoTurretCommand extends InstantCommand {
 
