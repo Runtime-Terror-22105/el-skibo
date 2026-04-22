3a9d118b7c8cc425f732f4493aeb88e8a6b69866
aaguy-hue <aaditmathur6@gmail.com>
2026-04-21 17:35:14 -0700

    intake positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/IntakeSubsystem.java
@@ -16,11 +16,11 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public static boolean debug = false;
 
-    public static double INTAKE_LEFT_DOWN = 0;
-    public static double INTAKE_LEFT_UP = 0;
+    public static double INTAKE_LEFT_DOWN = 0.55;
+    public static double INTAKE_LEFT_UP = 0.3;
 
-    public static double INTAKE_RIGHT_DOWN = 0;
-    public static double INTAKE_RIGHT_UP = 0;
+    public static double INTAKE_RIGHT_DOWN = 0.3;
+    public static double INTAKE_RIGHT_UP = 0.55;
 
     private double leftEffective = INTAKE_LEFT_UP;
     private double rightEffective = INTAKE_RIGHT_UP;
