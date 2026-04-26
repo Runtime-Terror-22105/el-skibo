34c420f02652a364c7811c86e34bbcd98826f72c
Jonathan Liu <jonliu168@gmail.com>
2026-04-25 19:25:20 -0700

    pideez hang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/HangSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/HangSubsystem.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
+import org.firstinspires.ftc.teamcode.math.controllers.PidfController;
 import org.firstinspires.ftc.teamcode.robot.init.Robot;
 import org.firstinspires.ftc.teamcode.robot.init.RobotHardware;
 import org.firstinspires.ftc.teamcode.robot.init.RobotState;
@@ -11,58 +12,51 @@ import org.firstinspires.ftc.teamcode.util.Profiler;
 
 @Config
 public class HangSubsystem extends SubsystemBase {
-//    public static double FIRST_ANGLE = Math.PI/2;
-//    public static double SECOND_ANGLE = Math.toRadians(75);
-//    public static double SERVO_POWER = 1.0;
-//    public static double HOLDING_POWER = 0.1;
-//    public static double FINAL_TOLERANCE = Math.toRadians(5);
-
     public static double PTO_ENGAGE_POSITION = 0;
     public static double PTO_DISENGAGE_POSITION = 0.4;
-
-    public static double PTO_INIT_POWER = -0.2;
     public static double PTO_RISE_POWER = -1;
-
-    public static boolean debug = true;
-
     public boolean isPTOEngaged = false;
-
     public ElapsedTime hangTimer = new ElapsedTime();
-
     public static double INIT_HANG_TIMER_MILLISECONDS = 250;
     public static double HANG_TIMER_MILLISECONDS = 4000;
+    public static double LEFT_PID_ANGLE = 0;
+    public static double RIGHT_PID_ANGLE = 0;
+
+    public static double LEFT_PID_TOLERANCE = 0;
+    public static double RIGHT_PID_TOLERANCE = 0;
 
     private final Robot robot;
     private final RobotHardware hardware;
 
+    public static PidfController.PidfCoefficients leftMotorPIDCoeff =
+            new PidfController.PidfCoefficients(0.35, 0, 0.015, 0, 0.12);
+
+    public static PidfController.PidfCoefficients rightMotorPIDCoeff =
+            new PidfController.PidfCoefficients(0.35, 0, 0.015, 0, 0.12);
+
+    public final PidfController leftMotorPID = new PidfController(leftMotorPIDCoeff);
+    public final PidfController rightMotorPID = new PidfController(rightMotorPIDCoeff);
+
     public HangSubsystem(RobotHardware hardware,Robot robot) {
         this.robot = robot;
         this.hardware = hardware;
-        setPTOEngagement(false);
-    }
-
-    public void setPTOEngagement(boolean state)
-    {
-        this.isPTOEngaged = state;
-    }
-    public boolean isPTOEngaged()
-    {
-        return isPTOEngaged;
+        this.leftMotorPID.setTargetPosition(LEFT_PID_ANGLE);
+        this.leftMotorPID.setTolerance(LEFT_PID_TOLERANCE);
+        this.rightMotorPID.setTargetPosition(RIGHT_PID_ANGLE);
+        this.rightMotorPID.setTolerance(RIGHT_PID_TOLERANCE);
     }
 
     public void beginHang()
     {
-        setPTOEngagement(true);
         robot.robotState = RobotState.HANG_INIT;
     }
 
     @Override
     public void periodic() {
 
-        hardware.pto.setPosition(isPTOEngaged ? PTO_ENGAGE_POSITION : PTO_DISENGAGE_POSITION);
-
-        if(!robot.robotState.isHang() || !isPTOEngaged())
+        if(!robot.robotState.isHang())
         {
+            hardware.pto.setPosition(PTO_DISENGAGE_POSITION);
             hangTimer.reset(); //could be cooked, lifes tough
             return;
         }
@@ -70,9 +64,16 @@ public class HangSubsystem extends SubsystemBase {
         switch(robot.robotState)
         {
             case HANG_INIT:
-                hardware.motorRearRight.setPower(PTO_INIT_POWER);
-                hardware.motorRearLeft.setPower(PTO_INIT_POWER);
-                if(hangTimer.milliseconds() > INIT_HANG_TIMER_MILLISECONDS)
+//                hardware.motorRearRight.setPower(PTO_INIT_POWER);
+//                hardware.motorRearLeft.setPower(PTO_INIT_POWER);
+
+                leftMotorPID.calculatePower(0,0);
+                rightMotorPID.calculatePower(0,0);
+
+                //i lowk dunno what rahul means by pid to a specific angle
+                //are there encoders on the motors or smth
+
+                if(hangTimer.milliseconds() > INIT_HANG_TIMER_MILLISECONDS || (leftMotorPID.atTargetPosition() && rightMotorPID.atTargetPosition()) )
                 {
                     this.robot.robotState =  RobotState.HANGING;
                     hangTimer.reset();
@@ -80,6 +81,7 @@ public class HangSubsystem extends SubsystemBase {
                 break;
 
             case HANGING:
+                hardware.pto.setPosition(PTO_ENGAGE_POSITION);
                 hardware.motorRearRight.setPower(PTO_RISE_POWER);
                 hardware.motorRearLeft.setPower(PTO_RISE_POWER);
                 if(hangTimer.milliseconds() > HANG_TIMER_MILLISECONDS)
