25b2f1d56f243491ae107731ff6eaadd0f8c715e
aaguy-hue <aaditmathur6@gmail.com>
2026-04-27 16:26:12 -0700

    whoops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/colored/Blue18.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/colored/Blue18.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.Team;
 import org.firstinspires.ftc.teamcode.opmodes.auto.Auto18;
 
-@Autonomous(name="Blue 18", group="Leave Auto")
+@Autonomous(name="🟦 Blue 18", group="18 Ball Auto")
 public class Blue18 extends Auto18 {
     public Blue18(){
         super(Team.BLUE);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/colored/Red18.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/colored/Red18.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.Team;
 import org.firstinspires.ftc.teamcode.opmodes.auto.Auto18;
 
-@Autonomous(name="Blue 18", group="Leave Auto")
+@Autonomous(name="🟥 Red 18", group="18 Ball Auto")
 public class Red18 extends Auto18 {
     public Red18(){
         super(Team.RED);
