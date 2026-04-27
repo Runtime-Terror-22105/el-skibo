75dd5523022b0f9253be3aeb2fbb74d3998542bd
aaguy-hue <aaditmathur6@gmail.com>
2026-04-27 16:23:26 -0700

    Add Red 18, tweak Auto18 to match our needs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/colored/Red18.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto.colored;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Team;
+import org.firstinspires.ftc.teamcode.opmodes.auto.Auto18;
+
+@Autonomous(name="Blue 18", group="Leave Auto")
+public class Red18 extends Auto18 {
+    public Red18(){
+        super(Team.RED);
+    }
+}
