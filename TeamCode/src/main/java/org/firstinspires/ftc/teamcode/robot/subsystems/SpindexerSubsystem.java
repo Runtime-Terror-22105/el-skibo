af9980ef3ae743aa51155102e42d08d6972e63db
aaguy-hue <aaditmathur6@gmail.com>
2026-04-26 20:45:52 -0700

    Tune spindex

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/SpindexerSubsystem.java
@@ -80,10 +80,10 @@ public class SpindexerSubsystem extends SubsystemBase {
     double[] yawOffsets = {0, (2.0 / 3) * Math.PI, -((2.0 / 3) * Math.PI)};
 
     public static PidfController.PidfCoefficients turningPidCoefficientsCcw =
-            new PidfController.PidfCoefficients(0.3, 0, 0.011, 0, 0.19);
+            new PidfController.PidfCoefficients(0.3, 0, 0.0115, 0, 0.12);
     public static PidfController.PidfCoefficients turningPidCoefficientsCw =
-            new PidfController.PidfCoefficients(0.3, 0, 0.011, 0, 0.17);
-    public static double yawPidTolerance = 0.12; // radians, used for kstatic
+            new PidfController.PidfCoefficients(0.25, 0, 0.012, 0, 0.15);
+    public static double yawPidTolerance = 0.08; // radians, used for kstatic
     public static double CHECKING_TOLERANCE = 0.075; // radians, only for checking if at target, not for PID tolerance
     private boolean pidEnabled = true;
     public final PidfController yawPid = new PidfController(turningPidCoefficientsCcw);
